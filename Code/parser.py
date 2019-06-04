9275ed4c28a515e9da35c0c434cefcfd7476bae2
Divij Bhatia <divijbhatia2@gmail.com>
2019-06-04 16:04:53 -0700

    added comments

--- Code/parser.py
+++ Code/parser.py
@@ -3,11 +3,13 @@ from lark import Lark
 #instantiate Lark Parser
 parser = Lark(open('grammar.lark'))
 
+#this function generates and prints the parse tree
 def generate_tree(program):
     parse_tree = parser.parse(program)
     print(parse_tree.pretty())
 
 def main():
+    #text contains the string to be parsed
     text = """
         value($col/3-n) = "SDFG" -> value(A/$row)
     """
