c4f173d58d4007538a1c97e4f59b06b1e9289485
Divij Bhatia <divijbhatia2@gmail.com>
2019-06-17 11:53:19 -0700

    bug fixed

--- Code/application.py
+++ Code/application.py
@@ -9,20 +9,20 @@ import os
 
 
 __CWD__ = os.getcwd()
-YAML_FILE = __CWD__ + "\\Datasets\\table-1a.yaml"
-EXCEL_FILE = __CWD__ + "\\Datasets\\homicide_report_total_and_sex.xlsx"
-EXCEL_SHEET_NAME = "table-1a"
-WIKIFIED_RESULT = __CWD__ + "\\Datasets\\wikified_result.csv"
+__YAML_FILE__ = __CWD__ + "\\Datasets\\table-1a.yaml"
+__EXCEL_FILE__ = __CWD__ + "\\Datasets\\homicide_report_total_and_sex.xlsx"
+__EXCEL_SHEET_NAME__ = "table-1a"
+__WIKIFIED_RESULT__ = __CWD__ + "\\Datasets\\wikified_result.csv"
 
 
-def add_excel_file_to_bindings() -> None:
+def add___EXCEL_FILE___to_bindings() -> None:
 	"""
 	This function reads the excel file and add the pyexcel object to the bindings
 	:return: None
 	"""
 	try:
-		records = pyexcel.get_book(file_name=EXCEL_FILE)
-		bindings["excel_sheet"] = records[EXCEL_SHEET_NAME]
+		records = pyexcel.get_book(file_name=__EXCEL_FILE__)
+		bindings["excel_sheet"] = records[__EXCEL_SHEET_NAME__]
 	except IOError:
 		print('Excel File cannot be found or opened')
 
@@ -34,20 +34,20 @@ def add_wikifier_result_to_bindings() -> None:
 	"""
 	try:
 		item_table = ItemTable()
-		item_table.generate_hash_tables(WIKIFIED_RESULT, True)
+		item_table.generate_hash_tables(__WIKIFIED_RESULT__, True)
 		bindings["item_table"] = item_table
 	except IOError:
 		print('Wikifier Result File cannot be found or opened')
 
 
 def main():
-	yaml_parser = YAMLParser(YAML_FILE)
+	yaml_parser = YAMLParser(__YAML_FILE__)
 	left, right, top, bottom = yaml_parser.get_region()
 	bindings["$left"] = utility_functions.get_excel_column_index(left)
 	bindings["$right"] = utility_functions.get_excel_column_index(right)
 	bindings["$top"] = utility_functions.get_excel_row_index(top)
 	bindings["$bottom"] = utility_functions.get_excel_row_index(bottom)
-	add_excel_file_to_bindings()
+	add___EXCEL_FILE___to_bindings()
 	add_wikifier_result_to_bindings()
 	region = Region(bindings["$left"], bindings["$right"], bindings["$top"], bindings["$bottom"])
 	data = []
@@ -56,7 +56,7 @@ def main():
 
 	while region.sheet.get((bindings["$col"], bindings["$row"]), None) is not None:
 		yaml_parser.resolve_template()
-		data.append({'row': bindings["$row"], 'column': bindings["$col"], 'statement': yaml_parser.get_template_in_json()})
+		data.append({'row': bindings["$row"], 'column': bindings["$col"], 'statement': yaml_parser.get_template()})
 		if region.sheet[(bindings["$col"], bindings["$row"])].next is not None:
 			(bindings["$col"], bindings["$row"]) = region.sheet[(bindings["$col"], bindings["$row"])].next
 		else:
