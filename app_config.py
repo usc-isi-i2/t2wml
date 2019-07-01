02b19800468af8a2ec0b5443538fd9b5b388bd2e
Jiasheng Wu <jiashenw@usc.edu>
2019-07-01 00:02:29 -0700

    Init GUI

--- app_config.py
+++ app_config.py
@@ -9,7 +9,7 @@ CODE_FOLDER = str(Path.cwd() / "Code")
 ETK_PATH = str(Path.cwd().parent / "etk")
 __user_files__ = defaultdict()
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="t2wml-gui")
 CORS(app)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
