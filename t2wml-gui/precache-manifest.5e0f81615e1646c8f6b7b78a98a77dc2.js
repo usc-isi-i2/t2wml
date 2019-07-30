5bfa3b2d25028cb2da8abfb1286b3425f2ee381c
Jiasheng Wu <jiashenw@usc.edu>
2019-07-30 15:08:36 -0700

    [GUI] Improve UX
    * Added logout button to start new session
    * Added pressing enter to add region
    * Fixed UI bugs in Safari

---
+++ t2wml-gui/precache-manifest.5e0f81615e1646c8f6b7b78a98a77dc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d99c79dc07a7d0c3ff18855fc07d82",
+    "url": "/t2wml-gui/editor.worker.js"
+  },
+  {
+    "revision": "f397472cd8a184187ec620362add2e15",
+    "url": "/t2wml-gui/index.html"
+  },
+  {
+    "revision": "d19846bd00096b97c730",
+    "url": "/t2wml-gui/static/css/2.6e35d814.chunk.css"
+  },
+  {
+    "revision": "babde601374172090245",
+    "url": "/t2wml-gui/static/css/main.828b04b8.chunk.css"
+  },
+  {
+    "revision": "d19846bd00096b97c730",
+    "url": "/t2wml-gui/static/js/2.1bb1960f.chunk.js"
+  },
+  {
+    "revision": "51e0a8e6bc6ebdce624b",
+    "url": "/t2wml-gui/static/js/3.2aaf83de.chunk.js"
+  },
+  {
+    "revision": "babde601374172090245",
+    "url": "/t2wml-gui/static/js/main.74e59b0f.chunk.js"
+  },
+  {
+    "revision": "1a63d104efb57ed08c41",
+    "url": "/t2wml-gui/static/js/runtime~main.8e7d7b30.js"
+  }
+]);
