03109e6e00342eb4198230d0e5726f6d361b099c
Jiasheng Wu <jiashenw@usc.edu>
2019-09-02 16:08:17 -0700

    [GUI] Implement most routes

--- t2wml-gui/service-worker.js
+++ t2wml-gui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/t2wml-gui/precache-manifest.54f689b95502061fae0e4ce309786df5.js"
+  "/t2wml-gui/precache-manifest.211e5d8402619b67c0a6576094c970f6.js"
 );
 
 self.addEventListener('message', (event) => {
