3f1b73c22438fb7cc3fa427ce6105bd1c622d690
Jiasheng Wu <jiashenw@usc.edu>
2019-09-03 11:20:12 -0700

    [GUI] Make logo home button

---
+++ t2wml-gui/precache-manifest.7aca3589b92e5e8e60ea388f4585029c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d99c79dc07a7d0c3ff18855fc07d82",
+    "url": "/t2wml-gui/editor.worker.js"
+  },
+  {
+    "revision": "e555a9942946d6d19829e4f6e38e2a62",
+    "url": "/t2wml-gui/home.html"
+  },
+  {
+    "revision": "201be730ec73e31afe259e061ba0cb52",
+    "url": "/t2wml-gui/index.html"
+  },
+  {
+    "revision": "3f244654a67bc3ce867ce61444ee97e8",
+    "url": "/t2wml-gui/login.html"
+  },
+  {
+    "revision": "c1bc3b70e3eade143205e6fece22e190",
+    "url": "/t2wml-gui/project.html"
+  },
+  {
+    "revision": "8d58dd4a029eb27651f5",
+    "url": "/t2wml-gui/static/css/10.2de5122b.chunk.css"
+  },
+  {
+    "revision": "e20a255035f7412c38c9",
+    "url": "/t2wml-gui/static/css/home.6f32e207.chunk.css"
+  },
+  {
+    "revision": "e4af50d3f80dbe0f0b71",
+    "url": "/t2wml-gui/static/css/index.828b04b8.chunk.css"
+  },
+  {
+    "revision": "fc4a083a66ab999943cc",
+    "url": "/t2wml-gui/static/css/login.24ce8830.chunk.css"
+  },
+  {
+    "revision": "38a60902687c53e990a6",
+    "url": "/t2wml-gui/static/css/project.419a33e5.chunk.css"
+  },
+  {
+    "revision": "f5e6de283e6fba399f86",
+    "url": "/t2wml-gui/static/js/0.611aba24.chunk.js"
+  },
+  {
+    "revision": "d9c766e1f3873d36a925",
+    "url": "/t2wml-gui/static/js/1.da79677a.chunk.js"
+  },
+  {
+    "revision": "8d58dd4a029eb27651f5",
+    "url": "/t2wml-gui/static/js/10.4daa2a40.chunk.js"
+  },
+  {
+    "revision": "1f33c23bc365825e11b0",
+    "url": "/t2wml-gui/static/js/11.5eea102c.chunk.js"
+  },
+  {
+    "revision": "e20a255035f7412c38c9",
+    "url": "/t2wml-gui/static/js/home.65362ce4.chunk.js"
+  },
+  {
+    "revision": "e4af50d3f80dbe0f0b71",
+    "url": "/t2wml-gui/static/js/index.5b48e8f9.chunk.js"
+  },
+  {
+    "revision": "fc4a083a66ab999943cc",
+    "url": "/t2wml-gui/static/js/login.5b89359e.chunk.js"
+  },
+  {
+    "revision": "38a60902687c53e990a6",
+    "url": "/t2wml-gui/static/js/project.4643dad9.chunk.js"
+  },
+  {
+    "revision": "3711fcefb5ca369faf5c",
+    "url": "/t2wml-gui/static/js/runtime~home.88f85130.js"
+  },
+  {
+    "revision": "81e72c6b8cce05f449b1",
+    "url": "/t2wml-gui/static/js/runtime~index.efe69a09.js"
+  },
+  {
+    "revision": "521167ef6c3a996ca3f8",
+    "url": "/t2wml-gui/static/js/runtime~login.a4fb9e24.js"
+  },
+  {
+    "revision": "0dca5bb0ef0041947272",
+    "url": "/t2wml-gui/static/js/runtime~project.1e71a71c.js"
+  }
+]);
--- t2wml-gui/service-worker.js
+++ t2wml-gui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/t2wml-gui/precache-manifest.6d26a2be9240f4d2ccef035a975ca356.js"
+  "/t2wml-gui/precache-manifest.7aca3589b92e5e8e60ea388f4585029c.js"
 );
 
 self.addEventListener('message', (event) => {
