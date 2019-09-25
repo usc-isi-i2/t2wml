fa7ca1a3c8322bb207664c16cd53231fc11a6f95
Jiasheng Wu <jiashenw@usc.edu>
2019-09-24 21:33:22 -0700

    [GUI] Fix showing wrong endpoint when reload

--- t2wml-gui/service-worker.js
+++ t2wml-gui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/t2wml-gui/precache-manifest.9f2f2da1645252e74aacf39465aa1dd6.js"
+  "/t2wml-gui/precache-manifest.625b101cd96fe798c263bd8d1c7d8d47.js"
 );
 
 self.addEventListener('message', (event) => {
