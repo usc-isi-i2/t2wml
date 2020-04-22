9c8d5d62d31f2b9f8f4a26216cff95edfa01c18e
devora <devorawitty@gmail.com>
2020-04-22 14:34:23 +0300

    parsing folder and some basic definitions

---
+++ backend_code/parsing/functions.py
@@ -0,0 +1,55 @@
+import ftfy
+
+
+def concat(*args):
+    return_str=""
+    for arg in args:
+        return_str+=arg
+    return return_str
+
+def substring(in_string, index1, index2=-1):
+    raise NotImplementedError
+
+def t_regex(in_string, reg, *num):
+    # extract a substring using a regex. The string is the regex and the result is the value of the first group in 
+    # the regex. If the regex contains no group, it is the match of the regex.
+    raise NotImplementedError
+
+def strip(input):
+    return str(input).strip()
+
+def lower(input):
+    return input.lower()
+
+def upper(input):
+    return input.upper()
+
+def title(input):
+    return input.title()
+
+def date_format(input, date_format):
+    raise NotImplementedError
+
+def instance_of(item, qnode):
+    raise NotImplementedError
+
+def clean(input):
+    return ftfy.fix_text(input)
+
+def split_index(*args):
+    raise NotImplementedError
+
+
+functions_dict=dict(
+    concat=concat, 
+    substring=substring,
+    t_regex=t_regex,
+    strip=strip,
+    lower=lower,
+    upper=upper,
+    title=title, 
+    date_format=date_format,
+    instance_of=instance_of,
+    clean=clean,
+    split_index=split_index
+)
