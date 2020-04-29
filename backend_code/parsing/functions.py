42b8743f662fce57200a8ea67361c0bc80856dc6
devora <devorawitty@gmail.com>
2020-04-29 12:51:39 +0300

    add some more function support

--- backend_code/parsing/functions.py
+++ backend_code/parsing/functions.py
@@ -1,43 +1,7 @@
 import ftfy
+import re
+from etk.wikidata.utils import parse_datetime_string
 
-
-def concat(*args):
-    return_str=""
-    for arg in args:
-        return_str+=arg
-    return return_str
-
-def substring(in_string, index1, index2=-1):
-    raise NotImplementedError
-
-def t_regex(in_string, reg, *num):
-    # extract a substring using a regex. The string is the regex and the result is the value of the first group in 
-    # the regex. If the regex contains no group, it is the match of the regex.
-    raise NotImplementedError
-
-def strip(input):
-    return str(input).strip()
-
-def lower(input):
-    return input.lower()
-
-def upper(input):
-    return input.upper()
-
-def title(input):
-    return input.title()
-
-def date_format(input, date_format):
-    raise NotImplementedError
-
-def instance_of(item, qnode):
-    raise NotImplementedError
-
-def clean(input):
-    return ftfy.fix_text(input)
-
-def split_index(*args):
-    raise NotImplementedError
 
 def contains(source, section):
     return section in str(source)
@@ -47,6 +11,53 @@ def starts_with(source, section):
 
 def ends_with(source, section):
     return str(source).endswith(section)
+
+def strip(input):
+    return str(input).strip()
+
+def lower(input):
+    return str(input).lower()
+
+def upper(input):
+    return str(input).upper()
+
+def title(input):
+    return str(input).title()
+
+def clean(input):
+    return ftfy.fix_text(str(input))
+
+def split_index(item, character, i):
+    # split a cell on some character and return the i-th value
+    # e.g., if D3 has “paul, susan, mike” then index(value(D/3), “,”, 3) returns mike
+    # this is a primitive version of a much more sophisticated feature we need to add later to deal with 
+    # cells that contain lists.
+    items=str(item).split(character)
+    return items[i-1] #1-indexed to 0-indexed
+
+
+def substring(input, start, end=-1):
+    #need to make decisions about how indexing of the substr will work (0 or 1 based, negative, etc)
+    return str(input)[start:end]
+
+def concat(*args):
+    # concatenate a list of expression, e.g., concat(value(D/$row), “, “, value(F/$row))
+    raise NotImplementedError
+
+def t_regex(in_string, reg, *num):
+    # extract a substring using a regex. The string is the regex and the result is the value of the first group in 
+    # the regex. If the regex contains no group, it is the match of the regex.
+    raise NotImplementedError
+
+def date_format(input, date_format):
+    raise NotImplementedError
+
+def instance_of(item, qnode):
+    raise NotImplementedError
+
+
+
+
 
 
 functions_dict=dict(
