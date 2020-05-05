0f2e2c97637bfae08e49ca601c60e6d9afeeec94
devora <devorawitty@gmail.com>
2020-05-05 13:39:28 +0300

    slightly refactor the classes, and fix CellRange

--- backend_code/parsing/classes.py
+++ backend_code/parsing/classes.py
@@ -4,18 +4,18 @@ from backend_code.spreadsheets.conversions import to_excel
 def index_converter(arg):
     try:
         if isinstance(arg, slice):
-            return slice(arg.start-1, arg.stop-1, arg.step)
+            return slice(arg.start-1, arg.stop, arg.step)
         return arg-1
     except Exception as e:
         raise e
 
-class ItemReturn:
-    def __init__(self, col, row, context):
-        item_table=bindings.item_table
+
+class ReturnClass:
+    def __init__(self, col, row, value=None):
         self.col=col
         self.row=row
-        self.value=item_table.get_item(self.col, self.row, context)
-
+        self.value=value #usually overwritten in children
+    
     def __eq__(self, comparator):
         if self.value==comparator:
             return True
@@ -26,14 +26,22 @@ class ItemReturn:
             pass
         return False
     
-    def __repr__(self):
-        return to_excel(self.col, self.row)+ " : "+str(self.value)
-    
     def __str__(self):
         return str(self.value)
     
     def __bool__(self):
         return bool(self.value)
+    
+    def __repr__(self):
+        return to_excel(self.col, self.row)+ " : "+str(self.value)
+
+
+class Item(ReturnClass):
+    def __init__(self, col, row, context):
+        super().__init__(col, row)
+        item_table=bindings.item_table
+        self.value=item_table.get_item(self.col, self.row, context)
+
 
 class ItemExpression:
     def __getitem__(self, args):
@@ -43,44 +51,61 @@ class ItemExpression:
             context='__NO_CONTEXT__'
         col=index_converter(args[0])
         row=index_converter(args[1])
-        return ItemReturn(col, row, context)
+        return Item(col, row, context)
 
-class Cell:
+class Cell(ReturnClass):
     def __init__(self, col, row):
+        super().__init__(col, row)
         data_sheet=bindings.excel_sheet
-        self.col=col
-        self.row=row
         self.value=data_sheet[row][col]
     
-    def __eq__(self, comparator):
-        return self.value==comparator
-    
-    def __repr__(self):
-        return to_excel(self.col, self.row)+ " : "+str(self.value)
-    
-    def __str__(self):
-        return str(self.value)
-    
-    def __bool__(self):
-        return bool(self.value)
 
 class CellRange:
     def __init__(self, col_args, row_args):
         data_sheet=bindings.excel_sheet
-        self.col_args=col_args
-        self.row_args=row_args
-        area=data_sheet[row_args][col_args]
-        if isinstance(row_args, slice):
-            area=[y for x in area for y in x]
-        self.area=area
+        self.col_args = col_args
+        self.row_args = row_args
+        rows_area = data_sheet[row_args]
+        if isinstance(row_args, int):
+            rows_area=[rows_area]
+
+        area=[]
+        for row in rows_area:
+            new_row=row[col_args]
+            area.append(new_row)
+
+        self.area = area
+        self.row_length=len(self.area[0])
+
+    @property
+    def flattened_area(self):
+        for row in self.area:
+            for col in row:
+                yield col
+
     def __eq__(self, comparator):
-        for i in self.area:
-            if i!=comparator:
+        for i in self.flattened_area:
+            if i != comparator:
                 return False
         return True
-    
+
     def __repr__(self):
         return str(self.area)
+
+    def __iter__(self):
+        for i in self.flattened_area:
+            yield i
+
+    def __setitem__(self, flat_index, data):
+        row=flat_index//self.row_length
+        col=flat_index%self.row_length
+        self.area[row][col]=data
+
+    def __getitem__(self, flat_index):
+        row=flat_index//self.row_length
+        col=flat_index%self.row_length
+        return self.area[row][col]
+
 
 class CellExpression:
     def __getitem__(self, item):
