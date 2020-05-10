69de6bfcf279956f9f6aa64ff020731c2ee6be42
devora <devorawitty@gmail.com>
2020-05-10 18:26:21 +0300

    begin adding support for pandas replacing pyexcel. nothing has changed in function except some slight reorganization

--- backend_code/spreadsheets/caching.py
+++ backend_code/spreadsheets/caching.py
@@ -1,7 +1,9 @@
 import os
+import pickle
 from pathlib import Path
 import pyexcel
-import pickle
+import pandas as pd
+
 
 def get_pickle_path(data_filepath, sheet_name):
     #moved outside of class so I can use it in file initalizer as well
@@ -17,39 +19,17 @@ class SheetCacher:
     def __init__(self, data_filepath, sheet_name):
         self.data_filepath=data_filepath
         self.sheet_name=sheet_name
-        self.load_sheet()
-    def load_sheet(self):
+        file_extension=Path(self.data_filepath).suffix
+        self.is_csv = True if file_extension.lower() == ".csv" else False
+        
+    def get_sheet(self):
         raise NotImplementedError
 
 
 class FileSystemPickle(SheetCacher):
-    def load_sheet(self):
-        if self.fresh_pickle():
-            self.data=self.load_pickle()
-        else:
-            #if not, load the sheet with pyexcel, save the pickle file for future use
-            self.data=self.load_spreadsheet_file()
-            self.pickle_sheet(self.data)
-        return self.data
-
     @property
     def pickle_path(self):
         return get_pickle_path(self.data_filepath, self.sheet_name)
-
-    def load_pickle(self):
-        #load the pickle file
-        with open(self.pickle_path, 'rb') as f:
-            data=pickle.load(f)
-        return data
-
-    def load_spreadsheet_file(self):
-        records=pyexcel.get_book_dict(file_name=self.data_filepath)
-        sheet=records[self.sheet_name]
-        return sheet
-
-    def pickle_sheet(self, data):
-        with open(self.pickle_path, 'wb') as f:
-            pickle.dump(data, f)
     
     def fresh_pickle(self):
         #checks if the pickle is "fresh"-- is more newly modified than the datafile
@@ -57,17 +37,83 @@ class FileSystemPickle(SheetCacher):
             if os.path.getmtime(self.pickle_path) > os.path.getmtime(self.data_filepath):
                 return True
         return False
+    
+    def get_sheet(self):
+        if self.fresh_pickle():
+            self.data=self.load_pickle()
+        else:
+            #if not, load the sheet with pyexcel, save the pickle file for future use
+            self.data=self.load_sheet()
+            self.pickle(self.data)
+        return self.data
+    
 
+class PyexcelFileSystemPickle(FileSystemPickle):
+    def load_pickle(self):
+        #load the pickle file
+        with open(self.pickle_path, 'rb') as f:
+            data=pickle.load(f)
+        return data
+    
+    def load_file(self):
+        return pyexcel.get_book_dict(file_name=self.data_filepath)
 
-def pickle_spreadsheet_file_and_get_sheet_names(data_filepath):
-    book_dict = pyexcel.get_book_dict(file_name=data_filepath)
-    sheet_names=[]
-    for sheet_name in book_dict:
-        sheet_names.append(sheet_name)
-        data=book_dict[sheet_name]
-        filepath=get_pickle_path(data_filepath, sheet_name)
-        with open(filepath, 'wb') as f:
+    def load_sheet(self, sheet_name):
+        records=self.load_file()
+        sheet=records[sheet_name]
+        return sheet
+
+    def pickle(self, data):
+        with open(self.pickle_path, 'wb') as f:
             pickle.dump(data, f)
-    return sheet_names
+    
+    @staticmethod
+    def save_file(data_filepath):
+        book_dict = pyexcel.get_book_dict(file_name=data_filepath)
+        sheet_names=[]
+        for sheet_name in book_dict:
+            sheet_names.append(sheet_name)
+            data=book_dict[sheet_name]
+            filepath=get_pickle_path(data_filepath, sheet_name)
+            with open(filepath, 'wb') as f:
+                pickle.dump(data, f)
+        return sheet_names
+
+
+class PandasFileSystemPickle(FileSystemPickle):
+    def load_pickle(self):
+        data=pd.read_pickle(self.pickle_path)
+        return data
+
+    def pickle(self, data):
+        pd.to_pickle(data, self.pickle_path)
+    
+    def load_file(self, sheet_name=None):
+        if self.is_csv:
+            data=pd.read_csv(self.data_filepath)
+        else:
+            data=pd.read_excel(self.data_filepath, sheet_name=sheet_name)
+        return data
+    
+    def load_sheet(self, sheet_name):
+        return self.load_file(sheet_name)
+    
+
+    @staticmethod
+    def save_file(data_filepath):
+        pickler=PandasFileSystemPickle(data_filepath, None)
+        xl=pickler.load_file()
+        if pickler.is_csv:
+            sheet_name=Path(data_filepath).name
+            pd.to_pickle(xl, get_pickle_path(data_filepath, sheet_name))
+            return [sheet_name]
+        else:
+            sheet_names=[]
+            for sheet_name in xl:
+                sheet_names.append(sheet_name)
+                df=xl[sheet_name]
+                pd.to_pickle(df, get_pickle_path(data_filepath, sheet_name))
+            return sheet_names
+
 
 
--- backend_code/spreadsheets/sheet.py
+++ backend_code/spreadsheets/sheet.py
@@ -1,13 +1,18 @@
-from backend_code.spreadsheets.caching import FileSystemPickle
+import pandas as pd
+import json
+from backend_code.spreadsheets.caching import PyexcelFileSystemPickle, PandasFileSystemPickle
 from backend_code.spreadsheets.conversions import to_excel
 import backend_code.t2wml_exceptions as T2WMLExceptions
+
+
+cache_class=PyexcelFileSystemPickle
 
 class Sheet:
     #a class to wrap ALL sheet interactions, so that all access to spreadsheet goes through here
     def __init__(self, data_filepath, sheet_name):
-        sc=FileSystemPickle(data_filepath, sheet_name)
-        self.data=sc.data
-        self.add_empty_row()
+        sc=cache_class(data_filepath, sheet_name)
+        self.data=sc.get_sheet()
+        #self.add_empty_row()
     
     def add_empty_row(self):
         empty_row= [""]*len(self.data[0])
@@ -24,3 +29,15 @@ class Sheet:
 
     def append(self, row):
         self.data.append(row)
+    
+    def to_json(self):
+        #rename cols
+        #rename rows
+        json_string=self.data.to_json(orient='table')
+        return_dict=json.loads(json_string)
+        return return_dict
+
+
+def save_and_get_sheet_names(data_filepath):
+    sheet_names= cache_class.save_file(data_filepath)
+    return sheet_names
