19ffc6b2f7e944a9091c83a0e360bc4b83c93af5
devora <devorawitty@gmail.com>
2020-06-10 13:01:46 +0300

    further optimizations: do not iter on n if there is no n cache region

--- backend/backend_code/cell_mapper.py
+++ backend/backend_code/cell_mapper.py
@@ -20,7 +20,9 @@ class Region:
         self.indices=OrderedDict()
         skip_rows=set(region_data.get("skip_row", []))
         skip_cols=set(region_data.get("skip_column", []))
-        skip_cells=set(region_data.get("skip_cell", []))
+        skip_cells=region_data.get("skip_cell", [])
+        skip_cells=[tuple(i) for i in skip_cells]
+        skip_cells=set(skip_cells)
         for column in range(self.left, self.right+1):
             if column not in skip_cols:
                 for row in range(self.top, self.bottom+1):
@@ -41,21 +43,21 @@ class Region:
         for key in self.indices:
             return key
 
-class MappingCacher:
-    def __init__(self, yaml_file_path, data_file_path, sheet_name):
+
+class Cacher:
+    def __init__(self, yaml_file_path,  data_file_path, sheet_name):
         self.yaml_file_path=yaml_file_path
         self.data_file_path=data_file_path
         self.sheet_name=sheet_name
-    
-    @property
-    def cache_path(self):
-            path=Path(self.yaml_file_path)
-            filename=path.stem+"_"+self.sheet_name+"_cached.json"
-            parent=path.parent
-            file_path=parent/"cache"
-            if not file_path.is_dir():
-                os.makedirs(file_path)
-            return str(file_path/filename)
+
+    def get_cache_path(self, title_str):
+        path=Path(self.yaml_file_path)
+        filename=path.stem+"_"+self.sheet_name+"_"+title_str+"_cached.json"
+        parent=path.parent
+        file_path=parent/"cache"
+        if not file_path.is_dir():
+            os.makedirs(file_path)
+        return str(file_path/filename)
 
     def is_fresh(self):
         if os.path.isfile(self.cache_path):
@@ -63,7 +65,36 @@ class MappingCacher:
                 os.path.getmtime(self.cache_path) > os.path.getmtime(self.data_file_path):
                 return True
         return False
+
+class RegionCacher(Cacher):
+    def __init__(self, yaml_file_path, data_file_path, sheet_name):
+        super().__init__(yaml_file_path, data_file_path, sheet_name)
     
+    @property
+    def cache_path(self):
+        return self.get_cache_path("region")
+
+    def load_from_cache(self):
+        if self.is_fresh():
+            with open(self.cache_path, 'r') as f:
+                region_data=json.load(f)
+            return Region(region_data)
+        return None
+    
+    def save(self, region_data):
+        with open(self.cache_path, 'w') as f:
+            json.dump(region_data, f)
+
+
+
+class MappingResultsCacher(Cacher):
+    def __init__(self, yaml_file_path, data_file_path, sheet_name):
+        super().__init__(yaml_file_path, data_file_path, sheet_name)
+    
+    @property
+    def cache_path(self):
+        return self.get_cache_path("result")
+        
     def save(self, highlight_data, statement_data):
         d={
             "highlight region": highlight_data,
@@ -96,16 +127,16 @@ class MappingCacher:
 class CellMapper:
     def __init__(self, yaml_file_path, item_table, data_file_path, sheet_name, sparql_endpoint, use_cache=False):
         self.yaml_data=validate_yaml(yaml_file_path)
-        
+        self.use_cache=use_cache
+
         try:
             sheet=Sheet(data_file_path, sheet_name)
         except IOError:
             raise IOError('Excel File cannot be found or opened')
         update_bindings(item_table=item_table, sheet=sheet, sparql_endpoint=sparql_endpoint)
         
-        region_parser=RegionParser(self.yaml_data)
-        self.region=Region(region_parser.parsed_region)
-        
+        self.init_region(yaml_file_path, data_file_path, sheet_name)
+
         template_parser=TemplateParser(self.yaml_data, self.region)
         self._template=template_parser.template
         self.eval_template=template_parser.eval_template
@@ -113,9 +144,22 @@ class CellMapper:
         self.sparql_endpoint=sparql_endpoint
         self.created_by=self.yaml_data['statementMapping'].get('created_by', 't2wml')
         
-        self.use_cache=use_cache
-        self.cacher=MappingCacher(yaml_file_path, data_file_path, sheet_name)
+        self.result_cacher=MappingResultsCacher(yaml_file_path, data_file_path, sheet_name)
     
+    def init_region(self, yaml_file_path, data_file_path, sheet_name):
+        region=None
+        if self.use_cache:
+            region_cacher=RegionCacher(yaml_file_path, data_file_path, sheet_name)
+            region=region_cacher.load_from_cache()
+            
+        if not region:
+            region_parser=RegionParser(self.yaml_data)
+            region=Region(region_parser.parsed_region)
+            if self.use_cache:
+                region_cacher.save(region_parser.parsed_region)
+
+        self.region=region
+
     @property
     def template(self):
         return deepcopy(self._template)
