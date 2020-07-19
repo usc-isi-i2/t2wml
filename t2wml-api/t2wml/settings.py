3a6d7f7cf2ff07849679bcede864f9c98981f693
devora <devorawitty@gmail.com>
2020-07-19 20:12:11 +0300

    remove defunct settings

--- t2wml-api/t2wml/settings.py
+++ t2wml-api/t2wml/settings.py
@@ -4,7 +4,6 @@ DEFAULT_SPARQL_ENDPOINT = 'https://dsbox02.isi.edu:8888/bigdata/namespace/wdq/sp
 
 t2wml_settings={
     "cache_data_files":False,
-    "cache_results":False,
     "sparql_endpoint": DEFAULT_SPARQL_ENDPOINT,
     "wikidata_provider": None, #default is SparqlProvider
     "storage_folder": (Path.cwd() / "storage")
