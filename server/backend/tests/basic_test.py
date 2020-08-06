aef499e953af20c00a9bc911574a060c4e3540c7
devora <devorawitty@gmail.com>
2020-08-06 18:14:11 +0300

    add a tiny bit more tests

--- server/backend/tests/basic_test.py
+++ server/backend/tests/basic_test.py
@@ -1,15 +1,10 @@
-from pathlib import Path
-import sys
-file_path=Path(__file__).parents[2] 
-sys.path.insert(0, file_path)
-fle_path=file_path / "backend"
-sys.path.insert(0, file_path)
-
-
+import json
 import os
 import tempfile
 import pytest
 from backend.app_config import app
+
+pid=None
 
 @pytest.fixture
 def client():
@@ -23,6 +18,83 @@ def client():
     os.close(db_fd)
     os.unlink(app.config['DATABASE'])
 
-def test_basic_get(client):
-    rv=client.get("/api/projects") 
-    assert rv.status_code==200
+
+def test_get_projects_list(client):
+    #GET /api/projects
+    response=client.get('/api/projects') 
+    data = response.data.decode("utf-8")
+    data = json.loads(data)
+    assert response.status_code==200
+
+def test_add_project(client):
+    #POST /api/project
+    response=client.post('/api/project',
+        data=dict(
+            ptitle="Unit test"
+        )
+    )
+    data = response.data.decode("utf-8")
+    data = json.loads(data)
+    global pid
+    pid=data['pid']
+    assert response.status_code==201
+
+def test_add_data_file(client):
+    print(pid)
+    #POST /api/data/<pid>
+    pass
+
+def test_add_properties_file(client):
+    #POST /api/project/<pid>/properties
+    pass
+
+def test_add_wikifier_file(client):
+    #POST '/api/wikifier/<pid>'
+    pass
+
+def test_change_sheet(client):
+    #GET /api/data/<pid>/<sheet_name>
+    pass
+
+def test_add_yaml_file(client):
+    #POST '/api/yaml/<pid>'
+    pass
+
+def test_get_project_files(client):
+    #GET /api/project/<pid>
+    pass
+
+def test_wikify_region(client):
+    #POST '/api/wikifier_service/<pid>'
+    pass
+
+def test_change_project_name(client):
+    #PUT '/api/project/<pid>'
+    pass
+
+def test_change_sparql_endpoint(client):
+    #PUT '/api/project/<pid>/sparql'
+    pass
+
+def test_delete_project(client):
+    #this test must be sequentially last (do not run pytest in parallel)
+    #DELETE '/api/project/<pid>'
+    url_str="/api/project/"+str(pid)
+    response=client.delete(url_str)
+    data = response.data.decode("utf-8")
+    data = json.loads(data)
+
+
+'''
+
+
+
+POST /api/project/<pid>/items
+GET /api/qnode/<qid>
+
+GET '/api/data/<pid>/cell/<col>/<row>'
+GET '/api/project/<pid>/download/<filetype>'
+
+
+
+'''
