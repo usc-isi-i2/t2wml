b6d35710dd3aef889b8f047a06a9428ed0e372c6
devora <devorawitty@gmail.com>
2020-08-06 09:09:33 +0300

    re-get rid of invalid classifier (was re-added accidentally)

--- server/setup.py
+++ server/setup.py
@@ -6,7 +6,6 @@ CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Environment :: Web Environment',
     'Framework :: Flask',
-	'Intended Audience :: Researchers',
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
