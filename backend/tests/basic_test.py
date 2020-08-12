a34798c6d18da050ac8ea67c548120c0d28a88ea
devora <devorawitty@gmail.com>
2020-08-12 14:45:40 +0300

    fiddle with sys path for tests on test server

--- backend/tests/basic_test.py
+++ backend/tests/basic_test.py
@@ -1,47 +1,12 @@
 import json
 import os
 
-from tests.utils import client
+from tests.utils import client, BaseClass
     
 
 pid=None #we need to use a global pid for some reason... self.pid does not work.
 
-class BaseClass:
-    files_dir=""
-    results_dict={} #used if we need to overwrite the existing results when something changes
-    expected_results_path=""
 
-    @property
-    def expected_results_dict(self):
-        try:
-            return self.e_results_dict
-        except AttributeError:
-            with open(self.expected_results_path, 'r', encoding="utf-8") as f:
-                expected_results_dict=json.load(f)
-            self.e_results_dict=expected_results_dict
-            return self.e_results_dict
-    
-    def recurse_lists_and_dicts(self, input1, input2):
-        if isinstance(input1, dict):
-            assert input1.keys()==input2.keys()
-            for key in input1:
-                self.recurse_lists_and_dicts(input1[key], input2[key])
-                
-        elif isinstance(input1, list):
-            assert len(input1)==len(input2)
-            for index, item in enumerate(input1):
-                self.recurse_lists_and_dicts(input1[index], input2[index])
-
-        assert input1==input2
-
-    def compare_jsons(self, data, expected_key):
-        expected_data=self.expected_results_dict[expected_key]
-        assert data.keys()==expected_data.keys()
-        for key in data:
-            try:
-                assert data[key]==expected_data[key]
-            except AssertionError as e:
-                self.recurse_lists_and_dicts(data[key], expected_data[key])
 
 
 class TestBasicWorkflow(BaseClass):
--- backend/tests/utils.py
+++ backend/tests/utils.py
@@ -1,15 +1,12 @@
 import tempfile
 import os
 import pytest
-
-# To import application we need to add the backend directory into sys.path
-import sys
-BACKEND_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-if BACKEND_DIR not in sys.path:
-    sys.path.append(BACKEND_DIR)
+import json
 
 from flask_migrate import upgrade
 from application import app
+
+BACKEND_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 @pytest.fixture(scope="class")
 def client(request):
@@ -25,3 +22,40 @@ def client(request):
 
     with app.test_client() as client:
         yield client
+
+class BaseClass:
+    files_dir=""
+    results_dict={} #used if we need to overwrite the existing results when something changes
+    expected_results_path=""
+
+    @property
+    def expected_results_dict(self):
+        try:
+            return self.e_results_dict
+        except AttributeError:
+            with open(self.expected_results_path, 'r', encoding="utf-8") as f:
+                expected_results_dict=json.load(f)
+            self.e_results_dict=expected_results_dict
+            return self.e_results_dict
+    
+    def recurse_lists_and_dicts(self, input1, input2):
+        if isinstance(input1, dict):
+            assert input1.keys()==input2.keys()
+            for key in input1:
+                self.recurse_lists_and_dicts(input1[key], input2[key])
+                
+        elif isinstance(input1, list):
+            assert len(input1)==len(input2)
+            for index, item in enumerate(input1):
+                self.recurse_lists_and_dicts(input1[index], input2[index])
+
+        assert input1==input2
+
+    def compare_jsons(self, data, expected_key):
+        expected_data=self.expected_results_dict[expected_key]
+        assert data.keys()==expected_data.keys()
+        for key in data:
+            try:
+                assert data[key]==expected_data[key]
+            except AssertionError as e:
+                self.recurse_lists_and_dicts(data[key], expected_data[key])
