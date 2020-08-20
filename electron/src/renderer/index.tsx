15104ef1b558712207563feda6e9b1ab7a9a86d7
Itay Zandbank <itay@chelem.co.il>
2020-08-21 00:44:32 +0300

    Add React to the splash screen. Add some info to the splash screen.

--- electron/src/renderer/index.tsx
+++ electron/src/renderer/index.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
+import Splash from './splash/Splash';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+// Check whether this is the splash screen or the main app
+//
+// Both windows share the same JS bundle. We may want to separate these into two bundles
+// in the future
+const splashId = document.getElementById('splash');
+if (splashId) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Splash />
+    </React.StrictMode>,
+    splashId
+  );
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
