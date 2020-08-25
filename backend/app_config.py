76f5aae98306135e77cb027eed288ed6d0ded242
saggu <amandeep.s.saggu@gmail.com>
2020-08-25 16:51:40 -0700

    update datamart api url

--- backend/app_config.py
+++ backend/app_config.py
@@ -39,7 +39,7 @@ app.config.from_object(AppConfig)
 
 DEFAULT_SPARQL_ENDPOINT = 'https://dsbox02.isi.edu:8888/bigdata/namespace/wdq/sparql'
 GOOGLE_CLIENT_ID = '552769010846-tpv08vhddblg96b42nh6ltg36j41pln1.apps.googleusercontent.com'
-DATAMART_API_ENDPOINT = 'http://localhost:14080'
+DATAMART_API_ENDPOINT = 'https://datamart:datamart-api-789@dsbox02.isi.edu:8888/datamart-api-wm'
 
 #############SQL STUFF
 
