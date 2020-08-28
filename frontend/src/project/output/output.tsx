51865ad921dd7ab175ed256fc024e90130b67a6f
Ke-Thia Yao <kyao@isi.edu>
2020-08-27 20:30:58 -0700

    use prompt box

--- frontend/src/project/output/output.tsx
+++ frontend/src/project/output/output.tsx
@@ -323,13 +323,15 @@ class Output extends Component<OutputProperties, OutputState> {
   loadToDatamart() {
     // TODO !
     wikiStore.output.showSpinner = true;
+    wikiStore.table.showSpinner = true;
     this.setState({ isLoadDatamart: true });
     console.log("Load to Datamart");
     this.requestService.loadToDatamart(this.pid).then((json) => {
         console.log(json);
         const { datamart_get_url, description } = json;
         if (datamart_get_url !== undefined) {
-            alert("Success! To download the data in canonical format use this url:\n" + datamart_get_url)
+            // alert("Success! To download the data in canonical format use this url:\n" + datamart_get_url)
+            prompt('Success! Use this url to download the data in canonical format:', datamart_get_url)
         } else {
             alert("Failed to load to Datamart\nError: " + description)
         }
@@ -342,6 +344,7 @@ class Output extends Component<OutputProperties, OutputState> {
     });
     this.setState({ isLoadDatamart: false });
     wikiStore.output.showSpinner = false;
+    wikiStore.table.showSpinner = true;
   }
 
   render() {
