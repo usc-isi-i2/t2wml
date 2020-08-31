fac60b2ac362a01b562b73713c762282d4852469
talyashra <talyashra@gmail.com>
2020-08-31 12:15:12 +0300

    handle open project

--- electron/src/renderer/App.tsx
+++ electron/src/renderer/App.tsx
@@ -9,7 +9,19 @@ import ProjectList from './project-list/project-list';
 import Project from './project/project';
 import { ipcRenderer, EventEmitter } from 'electron';
 
+
+// to open-project
+import { LOG, ErrorMessage } from './common/general';
+import RequestService from './common/service';
+import { useHistory } from 'react-router-dom';
+
+
 class App extends Component<{}, {}> {
+  private requestService: RequestService;
+  constructor(props: {}) {
+    super(props);
+    this.requestService = new RequestService();
+  }
 
   componentDidMount() {
     ipcRenderer.on('open-project', (sender: EventEmitter, folder: string) => {
@@ -26,6 +38,44 @@ class App extends Component<{}, {}> {
   
   onOpenProject(folder: string) {
     console.log('Opening project from folder ', folder);
+    this.handleOpenProject(folder);
+  }
+
+  async handleOpenProject(folder: string) {
+    this.setState({ errorMessage: {} as ErrorMessage });
+
+    // before sending request
+    this.setState({ showSpinner: true });
+
+    // send request
+    const formData = new FormData();
+    formData.append("path", folder);
+    try {
+      const response = await this.requestService.loadProject(formData);
+    
+      console.log("<App> <- %c/load_project%c with:", LOG.link, LOG.default);
+      console.log(response);
+
+      // do something here
+      if (response["pid"]) {
+        // success
+        const history = useHistory();
+        history.push("/project/" + response["pid"]);
+      } else {
+        // failure
+        throw Error("Session doesn't exist or invalid request");
+      }
+
+      // follow-ups (success)
+      this.setState({ showLoadProject: false, showSpinner: false });
+
+    } catch (error) { // :ErrorDEscription
+      error.errorDescription += "\n\nCannot load project!";
+      this.setState({ errorMessage: error });
+
+      // follow-ups (failure)
+      this.setState({ showLoadProject: false, showSpinner: false });
+    }
   }
 
   render() {
