fc60f0834a08f890371ab0bd1eb03b41359ae3fe
saggu <amandeep.s.saggu@gmail.com>
2020-09-14 09:51:29 -0700

    accept df or request

--- backend/t2wml_annotation_integration.py
+++ backend/t2wml_annotation_integration.py
@@ -1,22 +1,28 @@
+import re
 import tarfile
 import tempfile
 import pandas as pd
+from glob import glob
+from pathlib import Path
 from requests import post
 from app_config import DATAMART_API_ENDPOINT
 
 
 class AnnotationIntegration(object):
-    def __init__(self, w_requests, is_csv, sheet_name):
-        self.w_requests = w_requests
-        if not is_csv:
-            df = pd.read_excel(w_requests.files['file'], dtype=object, header=None, sheet_name=sheet_name).fillna('')
-        else:
-            df = pd.read_csv(w_requests.files['file'], dtype=object, header=None).fillna('')
+    def __init__(self, is_csv, sheet_name, w_requests=None, df=None):
+        if w_requests is None and df is None:
+            raise
+        if w_requests is not None:
+            if not is_csv:
+                df = pd.read_excel(w_requests.files['file'], dtype=object, header=None, sheet_name=sheet_name).fillna(
+                    '')
+            else:
+                df = pd.read_csv(w_requests.files['file'], dtype=object, header=None).fillna('')
         self.df = df
         self.dataset = None
         self.annotation = None
 
-    def is_annotated_spreadsheet(self):
+    def is_annotated_spreadsheet(self, project_path):
         # very basic check to see if the uploaded file looks like annotated file
         if self.df.iloc[0, 0].strip().lower() == 'dataset' \
                 and self.df.iloc[1, 0].strip().lower() == 'role' \
@@ -29,14 +35,64 @@ class AnnotationIntegration(object):
             header_row, data_row = self.find_data_start_row()
             annotation_rows = list(range(0, 6)) + [header_row]
             self.annotation = self.df.iloc[annotation_rows].fillna("")
+            self.save_annotation_file(project_path)
             return True
 
         return False
 
-    def get_files(self):
+    def save_annotation_file(self, project_path):
+        header_row = self.annotation.iloc[6]
+        header_str = '_'.join(header_row).lower().strip()
+
+        # basic cleanup
+        header_str = re.sub(r"\s+", '_', header_str)
+        header_str = header_str.replace("'", "")
+        header_str = header_str.replace("\"", "")
+        header_str = header_str.replace(",", "")
+        header_str = header_str.replace("/", "")
+        header_str = header_str.replace("\\", "")
+        header_str = header_str.replace("%", "")
+        header_str = header_str.replace("$", "")
+        header_str = header_str.replace("#", "")
+        header_str = header_str.replace("&", "")
+
+        # create the annotations folder for this project if it does not exist
+        Path(f'{project_path}/annotations').mkdir(parents=True, exist_ok=True)
+        self.annotation.to_csv(f'{project_path}/annotations/{header_str}.tsv', index=False, header=None, sep='\t')
+
+    def is_annotation_available(self, project_path):
+        # treat the first row as header as we don't know any better
+        input_file_columns = self.df.iloc[0]
+        annotation_path = Path(f'{project_path}/annotations')
+        annotation_found = True
+        new_df = None
+        if annotation_path.is_dir():  # annotations folder exists
+            ann_files = glob(f'{annotation_path}/*tsv')
+            for annotation_file in ann_files:
+                annotation_df = pd.read_csv(annotation_file, header=None, sep='\t').fillna('')
+                headers = list(annotation_df.iloc[6])
+
+                for c in input_file_columns:
+                    annotation_found = annotation_found and (c in headers)
+                if annotation_found:
+                    n_header = self.df.iloc[0]
+                    self.df.columns = n_header
+                    self.df = self.df.reindex(columns=headers).fillna('')
+                    annotation_df.columns = headers
+                    new_df = pd.concat([annotation_df.iloc[0:6], self.df])
+                    new_df.columns = range(new_df.shape[1])
+                    new_df.iloc[6,0] = 'header'
+                    new_df.iloc[7,0] = 'data'
+                    break
+        return annotation_found, new_df
+
+    def get_files(self, filename):
         temp_dir = tempfile.mkdtemp()
-        t_file = f'{temp_dir}/input_file_dm_t2wml.csv'
-        self.df.to_csv(t_file, index=False, header=None)
+        t_file = f'{temp_dir}/{filename}'
+        if filename.endswith(".csv"):
+            self.df.to_csv(t_file, index=False, header=None)
+        elif filename.endswith(".xlsx") or filename.endswith(".xls"):
+            self.df.to_excel(t_file, index=False, header=None)
 
         files = {
             'file': (t_file.split('/')[-1], open(t_file, mode='rb'), 'application/octet-stream')
@@ -48,6 +104,7 @@ class AnnotationIntegration(object):
             if "Error" in response.json():
                 raise ValueError(response.json()["Error"])
             else:
+                print(response.text)
                 raise ValueError("Some problem when communicating with datamart")
 
         with open('{}/t2wml_annotation_files.tar.gz'.format(temp_dir), 'wb') as d:
