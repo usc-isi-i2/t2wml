55439f9604d0c20342236cc5e02b6d99ebea0165
devora <devorawitty@gmail.com>
2020-09-16 12:43:19 +0300

    update tests for new, pidless reality (wip)

--- backend/tests/basic_test.py
+++ backend/tests/basic_test.py
@@ -5,16 +5,16 @@ from tests.utils import (client, BaseClass, create_project, sanitize_highlight_r
                 load_wikifier_file, load_item_file)
     
 
-pid=None #we need to use a global pid for some reason... self.pid does not work.
+pid=None #we need to use a global for some reason... self.project_folder does not work.
 
 
 
 
-class TestBasicWorkflow(BaseClass):
+class xTestBasicWorkflow(BaseClass):
     files_dir=os.path.join(os.path.dirname(__file__), "files_for_tests", "aid")
     expected_results_path=os.path.join(files_dir, "results.json")
 
-    def test_00_get_projects_list(self, client):
+    def test_00_get_projects_list_to_be_deprecated(self, client):
         #GET /api/projects
         response=client.get('/api/projects') 
         data = response.data.decode("utf-8")
@@ -28,8 +28,7 @@ class TestBasicWorkflow(BaseClass):
         
     
     def test_01b_change_project_name(self, client):
-        
-        url='/api/project/{pid}'.format(pid=pid)
+        url='/api/project?project_folder={pid}'.format(pid=pid)
         ptitle="Unit test"
         response=client.put(url,
                 data=dict(
@@ -96,28 +95,16 @@ class TestBasicWorkflow(BaseClass):
 
     def test_09_get_cell(self, client):
         #GET '/api/data/{pid}/cell/<col>/<row>'
-        url='/api/data/{pid}/cell/{col}/{row}'.format(pid=pid, col="G", row=4)
+        url='/api/data/cell/{col}/{row}?project_folder={pid}'.format(pid=pid, col="G", row=4)
         response=client.get(url) 
         data = response.data.decode("utf-8")
         data = json.loads(data)
         self.results_dict['get_cell']=data
         self.compare_jsons(data, 'get_cell')
 
-    def test_10_get_node(self, client):
-        url='/api/qnode/{pid}/{qid}'.format(pid=pid, qid="Q21203")
-        response=client.get(url) 
-        data = response.data.decode("utf-8")
-        data = json.loads(data)
-        assert data['label']=='Aruba'
-        url='/api/qnode/{pid}/{qid}'.format(pid=pid, qid="P17")
-        response=client.get(url) 
-        data2 = response.data.decode("utf-8")
-        data2 = json.loads(data2)
-        assert data2['label']=='country'
-
     def test_11_get_download(self, client):
         #GET '/api/project/{pid}/download/<filetype>'
-        url='/api/project/{pid}/download/{filetype}'.format(pid=pid, filetype="tsv")
+        url='/api/project/download/{filetype}?project_folder={pid}'.format(pid=pid, filetype="tsv")
         response=client.get(url) 
         data = response.data.decode("utf-8")
         data = json.loads(data)
@@ -128,7 +115,7 @@ class TestBasicWorkflow(BaseClass):
 
     def test_12_change_sheet(self, client):
         #GET /api/data/{pid}/<sheet_name>
-        url='/api/data/{pid}/{sheet_name}'.format(pid=pid,sheet_name="Sheet4")
+        url='/api/data/{sheet_name}?project_folder={pid}'.format(pid=pid,sheet_name="Sheet4")
         response=client.get(url) 
         data = response.data.decode("utf-8")
         data = json.loads(data)
@@ -139,7 +126,7 @@ class TestBasicWorkflow(BaseClass):
 
     def test_12_wikify_region(self, client):
         #POST '/api/wikifier_service/{pid}'
-        url='/api/wikifier_service/{pid}'.format(pid=pid)
+        url='/api/wikifier_service?project_folder={pid}'.format(pid=pid)
         response=client.post(url,
                 data=dict(
                 action="wikify_region",
@@ -158,7 +145,7 @@ class TestBasicWorkflow(BaseClass):
     def test_14_settings(self, client):
         from t2wml.settings import t2wml_settings
         #PUT '/api/project/{pid}/settings'
-        url='/api/project/{pid}/settings'.format(pid=pid)
+        url='/api/project/settings?project_folder={pid}'.format(pid=pid)
         endpoint='https://query.wikidata.org/bigdata/namespace/wdq/sparql'
         response=client.put(url,
                 data=dict(
@@ -168,7 +155,7 @@ class TestBasicWorkflow(BaseClass):
         assert t2wml_settings.wikidata_provider.sparql_endpoint==endpoint
 
         #GET '/api/project/{pid}/settings'
-        url='/api/project/{pid}/settings'.format(pid=pid)
+        url='/api/project/settings?project_folder={pid}'.format(pid=pid)
         response=client.get(url) 
         data = response.data.decode("utf-8")
         data = json.loads(data)
@@ -178,14 +165,16 @@ class TestBasicWorkflow(BaseClass):
     def test_99_delete_project(self, client):
         #this test must be sequentially last (do not run pytest in parallel)
         #DELETE '/api/project/{pid}'
-        url_str="/api/project/{pid}".format(pid=pid)
+        url_str="/api/project?project_folder={pid}".format(pid=pid)
         response=client.delete(url_str)
         data = response.data.decode("utf-8")
         data = json.loads(data)
-        #used when overwriting all old results with new ones 
-        #with open(self.expected_results_path, 'w') as f:
-        #    json.dump(self.results_dict, f, sort_keys=False, indent=4)
         assert data["projects"]==[]
+    
+    def xtest_999_save(self):
+        #used when overwriting all old results with new ones 
+        with open(self.expected_results_path, 'w') as f:
+            json.dump(self.results_dict, f, sort_keys=False, indent=4)
 
 
 
@@ -194,25 +183,19 @@ class TestLoadingProject(BaseClass):
     expected_results_path=os.path.join(files_dir, "project_results.json")
 
     def test_10_load_from_path(self, client):
-        response=client.post('/api/project/load',
-        data=dict(
-                path=self.files_dir
-            )
-        )
+        url='/api/project/load?project_folder={path}'.format(path=self.files_dir)
+        response=client.post(url)
         data = response.data.decode("utf-8")
         data = json.loads(data)
-        global pid
-        pid=str(data['pid'])
         assert response.status_code==201
     
     def test_11_get_loaded_yaml_files(self, client):
-        url= '/api/project/{pid}'.format(pid=pid)
+        url= '/api/project?project_folder={path}'.format(path=self.files_dir)
         response=client.get(url)
         data = response.data.decode("utf-8")
         data = json.loads(data)
         data.pop('project')
         self.results_dict['load_from_path']=data
-
         #some of the results are sent back as unordered lists and need to be compared separately
         set_keys=[]
         dict_1=data["yamlData"]["yamlRegions"]
