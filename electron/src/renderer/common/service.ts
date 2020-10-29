ae7719da99bd22900def2571771c9e60b849c584
Talya Klein <talya@researchsoftware.co.il>
2020-10-29 13:15:47 +0200

    Extends in DTO interfaces, fillStore function in the service.

--- electron/src/renderer/common/service.ts
+++ electron/src/renderer/common/service.ts
@@ -1,7 +1,7 @@
 import wikiStore from '../data/store';
 import { backendGet, backendPost, backendPut } from './comm';
 import { GetProjectResponseDTO, ProjectDTO, UploadDataFileResponseDTO, UploadWikifierOutputResponseDTO, 
-  UploadYamlResponseDTO, UploadEntitiesDTO, CallWikifierServiceDTO, LayersDTO } from './dtos';
+  UploadYamlResponseDTO, UploadEntitiesDTO, CallWikifierServiceDTO, TableDTO, LayersDTO, ChangeSheetResponseDTO, ResponseWithLayersDTO } from './dtos';
 
 // I did it as a class because we will add a state instance
 
@@ -9,27 +9,27 @@ class RequestService {
 
   public async createProject(folder: string) {
     const response = await backendPost(`/project?project_folder=${folder}`) as ProjectDTO;
-    wikiStore.projects.projectDTO = response;
+    this.fillProjectInStore(response);
   }
   
   public async uploadDataFile(folder: string, formData: any) {
     const response = await backendPost(`/data?project_folder=${folder}`, formData) as UploadDataFileResponseDTO;
-    this.fillStore(response);
+    this.fillUploadDataInStore(response);
   }
 
   public async changeSheet(folder: string, sheetName: string) {
-    const response = await backendGet(`/data/${sheetName}?project_folder=${folder}`) as GetProjectResponseDTO;
-    this.fillStore(response);
+    const response = await backendGet(`/data/${sheetName}?project_folder=${folder}`) as ChangeSheetResponseDTO;
+    this.fillgetProjectData(response);
   }
 
   public async uploadWikifierOutput(folder: string, formData: any) {
     const response = await backendPost(`/wikifier?project_folder=${folder}`, formData) as UploadWikifierOutputResponseDTO;
-    this.fillStore(response);
+    this.fillProjectAndLayers(response);
   }
 
   public async uploadYaml(folder: string, formData: any) {
     const response = await backendPost(`/yaml?project_folder=${folder}`, formData) as UploadYamlResponseDTO;
-    this.fillStore(response);
+    this.fillProjectAndLayers(response);
   }
 
   public async downloadResults(folder: string, fileType: string) {
@@ -42,30 +42,30 @@ class RequestService {
     //returns project, rowData, qnodes
     //also returns problemCells (an error dict, or False)
     const response = await backendPost(`/wikifier_service?project_folder=${folder}`, formData) as CallWikifierServiceDTO;
-    this.fillStore(response);
+    this.fillCallWikifier(response);
   }
 
   public async getProject(folder: string) {
     const response = await backendGet(`/project?project_folder=${folder}`) as GetProjectResponseDTO;
-    this.fillStore(response);
+    this.fillgetProjectData(response);
   }
 
   public async renameProject(folder: string, formData: any) {
     //returns project
     const response = await backendPut(`/project?project_folder=${folder}`, formData) as ProjectDTO;
-    wikiStore.projects.projectDTO = response;
+    this.fillProjectInStore(response);
   }
 
   public async getSettings(folder: string, formData: any) {
     //returns endpoint, warnEmpty
     const response = await backendPut(`/project/settings?project_folder=${folder}`, formData) as ProjectDTO;
-    wikiStore.projects.projectDTO = response;
+    this.fillProjectInStore(response);
   }
 
   public async uploadEntities(folder: string, formData: any) {
     //returns "widget", "project", "rowData", "qnodes"
     const response = await backendPost(`/project/entity?project_folder=${folder}`, formData) as UploadEntitiesDTO;
-    this.fillStore(response);
+    this.fillEntitiesData(response);
   }
   
   public async loadToDatamart(folder: string) {
@@ -74,27 +74,46 @@ class RequestService {
     return response;
   }
 
-  public fillStore(response: GetProjectResponseDTO | UploadYamlResponseDTO | UploadDataFileResponseDTO | UploadWikifierOutputResponseDTO) {
-    // Talya - this is a bad way of writing this. You should probably have one fillStore function per DTO type. Or use inheritence to share
-    // properties among DTOs - look at the example in dtos.ts
-    wikiStore.projects.projectDTO = response.project;
-    wikiStore.layers.updateFromDTO(response.layers)  // respone.layers should be LayersDTO, instead of casting
 
-    if ((response as GetProjectResponseDTO).yamlContent) {
-      wikiStore.yaml.yamlContent = (response as GetProjectResponseDTO).yamlContent;
-    }
+  public fillProjectInStore(project: ProjectDTO) {
+    wikiStore.projects.projectDTO = project;
+  }
+  public fillTableInStore(table: TableDTO) {
+    wikiStore.table.table = table;
+  }
+  public fillLayersInStore(layers: LayersDTO) {
+    wikiStore.layers.updateFromDTO(layers)  // respone.layers should be LayersDTO, instead of casting
+  }
+  public fillYamlContentInStore(content: string) {
+    wikiStore.yaml.yamlContent = content;
+  }
 
-    if ((response as GetProjectResponseDTO).table) {
-      wikiStore.table.table = (response as GetProjectResponseDTO).table;
-    }
 
-    if ((response as CallWikifierServiceDTO).wikifierError) {
-      wikiStore.wikifier.wikifierError = (response as CallWikifierServiceDTO).wikifierError;
-    }
 
-    if ((response as UploadEntitiesDTO).entitiesStats) {
-      wikiStore.entitiesStats = (response as UploadEntitiesDTO).entitiesStats;
-    }
+  public fillUploadDataInStore(response: UploadDataFileResponseDTO) {
+    this.fillProjectInStore(response.project);
+    this.fillTableInStore(response.table);
+    this.fillLayersInStore(response.layers);
+  }
+
+  public fillgetProjectData(response: GetProjectResponseDTO) {
+    this.fillUploadDataInStore(response);
+    this.fillYamlContentInStore(response.yamlContent);
+  }
+
+  public fillProjectAndLayers(response: ResponseWithLayersDTO) {
+    this.fillProjectInStore(response.project);
+    this.fillLayersInStore(response.layers);
+  }
+
+  public fillCallWikifier(response: CallWikifierServiceDTO) {
+    this.fillProjectAndLayers(response);
+    wikiStore.wikifier.wikifierError = response.wikifierError;
+  }
+
+  public fillEntitiesData(response: UploadEntitiesDTO) {
+    this.fillProjectAndLayers(response);
+    wikiStore.entitiesStats = response.entitiesStats;
   }
 }
 
