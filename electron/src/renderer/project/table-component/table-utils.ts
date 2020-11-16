b3e8b9f9ee2c138f9b192b998709ac34cc5a863e
g1eb <mail@g1eb.com>
2020-11-16 00:24:34 -0800

    Add a separate file for the table related utility functions

---
+++ electron/src/renderer/project/table-component/table-utils.ts
@@ -0,0 +1,17 @@
+export function columnToLetter(column) {
+  let temp, letter = '';
+  while ( column > 0 ) {
+    temp = (column - 1) % 26;
+    letter = String.fromCharCode(temp + 65) + letter;
+    column = (column - temp - 1) / 26;
+  }
+  return letter;
+}
+
+export function letterToColumn(letter) {
+  let column = 0, length = letter.length;
+  for ( let i = 0; i < length; i++ ) {
+    column += (letter.charCodeAt(i) - 64) * Math.pow(26, length - i - 1);
+  }
+  return column;
+}
