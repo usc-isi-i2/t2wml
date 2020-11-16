0664508adb910c4720767ec180bba565adbfce2d
g1eb <mail@g1eb.com>
2020-11-15 19:16:35 -0800

    Code cleanup for consistent use of spaces and single/double quotes

--- electron/src/renderer/project/table-component/table-legend.tsx
+++ electron/src/renderer/project/table-component/table-legend.tsx
@@ -1,49 +1,59 @@
 import React, { Component } from 'react';
 
 // icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuestion } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
 
 // App
 import { Button, OverlayTrigger, Popover } from 'react-bootstrap';
 
-import { observer } from "mobx-react";
+import { observer } from 'mobx-react';
 
 interface LegendProperties {
-    multipleSheets: boolean;
+  multipleSheets: boolean;
 }
+
+
+const LEGEND = {
+
+}
+
 
 @observer
 class TableLegend extends Component<LegendProperties, {}> {
 
   renderLegend() {
     return (
-      <Popover className="shadow" style={{ backgroundColor: "rgba(255,255,255,0.8)" }} id="table">
-        <div style={{ margin: "10px 30px" }}>
+      <Popover className="shadow" style={{ backgroundColor: 'rgba(255,255,255,0.8)' }}>
+        <div style={{ margin: '10px 30px' }}>
           <span><strong>Legend</strong>:&nbsp;</span>
-          <span className="legend" style={{ backgroundColor: "white", color: "hsl(200, 100%, 30%)", marginLeft: "0" }}>wikified</span>
-          <span className="legend" style={{ backgroundColor: "hsl(200, 50%, 90%)" }}>item</span>
-          <span className="legend" style={{ backgroundColor: "hsl(250, 50%, 90%)" }}>qualifier</span>
-          <span className="legend" style={{ backgroundColor: "hsl(150, 50%, 90%)" }}>data</span>
-          <span className="legend" style={{ backgroundColor: "hsl(0, 0%, 90%)" }}>data&nbsp;(skipped)</span>
+          <span className="legend" style={{ backgroundColor: 'white', color: 'hsl(200, 100%, 30%)', marginLeft: '0' }}>wikified</span>
+          <span className="legend" style={{ backgroundColor: 'hsl(200, 50%, 90%)' }}>item</span>
+          <span className="legend" style={{ backgroundColor: 'hsl(250, 50%, 90%)' }}>qualifier</span>
+          <span className="legend" style={{ backgroundColor: 'hsl(150, 50%, 90%)' }}>data</span>
+          <span className="legend" style={{ backgroundColor: 'hsl(0, 0%, 90%)' }}>data&nbsp;(skipped)</span>
         </div>
       </Popover>
     );
   }
 
   render() {
-      return (
-          <OverlayTrigger overlay={this.renderLegend()} trigger={["hover", "focus"]} placement="left">
-          <Button
-            className="myPopover shadow"
-            variant="secondary"
-            style={this.props.multipleSheets ? { cursor: "default", bottom: "70px" }: { cursor: "default" } }
-          >
-            <FontAwesomeIcon icon={faQuestion} />
-          </Button>
-        </OverlayTrigger>
-      );
+    const { multipleSheets } = this.props;
+    return (
+      <OverlayTrigger
+        placement="left"
+        trigger={['hover', 'focus']}
+        overlay={this.renderLegend()}>
+        <Button
+          variant="secondary"
+          className="myPopover shadow"
+          style={multipleSheets ? { cursor: 'default', bottom: '70px' } : { cursor: 'default' } }>
+          <FontAwesomeIcon icon={faQuestion} />
+        </Button>
+      </OverlayTrigger>
+    );
   }
 }
+
 
 export default TableLegend;
