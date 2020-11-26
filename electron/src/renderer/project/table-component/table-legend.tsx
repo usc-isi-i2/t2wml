69935c36ab4d69776b356e0c1fe7ecc4613123ad
g1eb <mail@g1eb.com>
2020-11-25 22:28:00 -0800

    Swap the order of metadata and property in the table legend component

--- electron/src/renderer/project/table-component/table-legend.tsx
+++ electron/src/renderer/project/table-component/table-legend.tsx
@@ -25,11 +25,11 @@ const LEGEND = [{
   label: 'reference',
   className: 'type-reference',
 }, {
-  label: 'property',
-  className: 'type-property',
-}, {
   label: 'metadata',
   className: 'type-metadata',
+}, {
+  label: 'property',
+  className: 'type-property',
 }, {
   label: 'minorError',
   className: 'type-minorError',
