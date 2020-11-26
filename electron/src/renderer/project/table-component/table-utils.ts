efced769804c69651ced5b6808cad2edf280736e
g1eb <mail@g1eb.com>
2020-11-26 02:45:54 -0800

    ESLint fixes, should be `const` instead of `let`, never reassigned

--- electron/src/renderer/project/table-component/table-utils.ts
+++ electron/src/renderer/project/table-component/table-utils.ts
@@ -9,7 +9,7 @@ export function columnToLetter(column) {
 }
 
 export function letterToColumn(letter) {
-  let column = 0, length = letter.length;
+  const column = 0, length = letter.length;
   for ( let i = 0; i < length; i++ ) {
     column += (letter.charCodeAt(i) - 64) * Math.pow(26, length - i - 1);
   }
