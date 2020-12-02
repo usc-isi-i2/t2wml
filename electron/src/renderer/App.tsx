739c2ae6d2c7e6f1d7a5acaa8c3c6329e9729056
g1eb <mail@g1eb.com>
2020-12-02 11:31:34 -0800

    Import the fs library instead of using require()

--- electron/src/renderer/App.tsx
+++ electron/src/renderer/App.tsx
@@ -10,6 +10,7 @@ import { ErrorMessage, LOG } from './common/general';
 import ToastMessage from './common/toast';
 import { Spinner } from 'react-bootstrap';
 import { IpcRendererEvent } from 'electron/renderer';
+import * as fs from 'fs';
 
 
 interface AppState extends IStateWithError {
@@ -45,7 +46,6 @@ class App extends Component<{}, AppState> {
   }
 
   checkCommandLineArgs() {
-    var fs = require('fs');
     const commandArgs = remote.getGlobal('sharedObj').prop1;
     const lastArg = commandArgs[commandArgs.length - 1];
     let projectDir = null;
