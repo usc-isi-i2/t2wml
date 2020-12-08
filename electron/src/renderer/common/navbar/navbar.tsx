f312f1a5c2c7a265d74ffb7e0c5d46afc0947e53
g1eb <mail@g1eb.com>
2020-12-08 10:19:06 -0800

    Remove trailing whitespace, indentation fix

--- electron/src/renderer/common/navbar/navbar.tsx
+++ electron/src/renderer/common/navbar/navbar.tsx
@@ -13,26 +13,28 @@ interface NavbarProperteis {
   onShowSettingsClicked?: Function;
 }
 
+
 class T2wmlNavbar extends Component<NavbarProperteis> {
   render() {
     return (
       <div>
+
         {/* navbar */}
         <Navbar className="shadow" bg="dark" variant="dark" style={{ height: "50px" }}> { /* Move to Navbar.css */ }
 
-            {/* logo */}
-            <T2WMLLogo />
+          {/* logo */}
+          <T2WMLLogo />
 
-            {/* name */}
-            <Navbar.Text className="pr-2" style={{ cursor: "default", fontWeight: 'bold', right: '50%', position: 'absolute' }}>
-              {this.props.name}
-            </Navbar.Text>
-          
+          {/* name */}
+          <Navbar.Text className="pr-2" style={{ cursor: "default", fontWeight: 'bold', right: '50%', position: 'absolute' }}>
+            {this.props.name}
+          </Navbar.Text>
 
         </Navbar>
       </div>
     );
   }
 }
+
 
 export default T2wmlNavbar;
