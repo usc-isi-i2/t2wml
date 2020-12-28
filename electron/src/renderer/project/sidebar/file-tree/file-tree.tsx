16dc8d1245b09a44a358e26fea615b39e6609d0c
devora <devorawitty@gmail.com>
2020-12-28 15:32:48 +0200

    indent with 2 spaces

--- electron/src/renderer/project/sidebar/file-tree/file-tree.tsx
+++ electron/src/renderer/project/sidebar/file-tree/file-tree.tsx
@@ -14,69 +14,69 @@ interface TreeState {
 
 class FileTree extends Component<TreeProps, TreeState> {
 
-    getSubFileTree(projDict: any, df: string, sheetName: string, label: string, type: NodeType, parentNode: NodeProps) {
-        if (!projDict[df] || !projDict[df][sheetName]) {
-            return;
+  getSubFileTree(projDict: any, df: string, sheetName: string, label: string, type: NodeType, parentNode: NodeProps) {
+    if (!projDict[df] || !projDict[df][sheetName]) {
+      return;
+    }
+    const labelNode = {
+      label: label,
+      childNodes: [],
+      parentNode: null,
+      type: "Label"
+    } as NodeProps;
+
+    for (const filename of projDict[df][sheetName]["val_arr"]) {
+      labelNode.childNodes.push(
+        {
+          label: filename,
+          childNodes: [] as NodeProps[],
+          type: type,
+          parentNode: parentNode
         }
-        const labelNode = {
-            label: label,
-            childNodes: [],
-            parentNode: null,
-            type: "Label"
+      )
+    }
+    parentNode.childNodes.push(labelNode);
+  }
+
+  getFileTree(): NodeProps {
+    const rootNode = { label: "Files", childNodes: [], type: "Label", parentNode: null } as NodeProps;
+    const project = wikiStore.projects.projectDTO;
+    if (!project || !project.data_files) { return rootNode; }
+    for (const df of Object.keys(project.data_files).sort()) {
+      const dataNode = {
+        label: df,
+        childNodes: [],
+        type: "Datafile",
+        parentNode: rootNode
+      } as NodeProps;
+      const sheet_arr = project.data_files[df].val_arr;
+      for (const sheetName of sheet_arr) {
+        const sheetNode = {
+          label: sheetName,
+          childNodes: [],
+          type: "Sheet",
+          parentNode: dataNode
         } as NodeProps;
-
-        for (const filename of projDict[df][sheetName]["val_arr"]) {
-            labelNode.childNodes.push(
-                {
-                    label: filename,
-                    childNodes: [] as NodeProps[],
-                    type: type,
-                    parentNode: parentNode
-                }
-            )
-        }
-        parentNode.childNodes.push(labelNode);
+        this.getSubFileTree(project.yaml_sheet_associations, df, sheetName, "Yaml files", "Yaml", sheetNode)
+        this.getSubFileTree(project.annotations, df, sheetName, "Annotation files", "Annotation", sheetNode)
+        dataNode.childNodes.push(sheetNode)
+      }
+      rootNode.childNodes.push(dataNode)
     }
+    return rootNode;
+  }
 
-    getFileTree(): NodeProps {
-        const rootNode = { label: "Files", childNodes: [], type: "Label", parentNode: null } as NodeProps;
-        const project = wikiStore.projects.projectDTO;
-        if (!project || !project.data_files) { return rootNode; }
-        for (const df of Object.keys(project.data_files).sort()) {
-            const dataNode = {
-                label: df,
-                childNodes: [],
-                type: "Datafile",
-                parentNode: rootNode
-            } as NodeProps;
-            const sheet_arr = project.data_files[df].val_arr;
-            for (const sheetName of sheet_arr) {
-                const sheetNode = {
-                    label: sheetName,
-                    childNodes: [],
-                    type: "Sheet",
-                    parentNode: dataNode
-                } as NodeProps;
-                this.getSubFileTree(project.yaml_sheet_associations, df, sheetName, "Yaml files", "Yaml", sheetNode)
-                this.getSubFileTree(project.annotations, df, sheetName, "Annotation files", "Annotation", sheetNode)
-                dataNode.childNodes.push(sheetNode)
-            }
-            rootNode.childNodes.push(dataNode)
-        }
-        return rootNode;
-    }
-
-    render() {
-        const fileTree = this.getFileTree()
-        return (
-            <ul>
-            <FileNode label={fileTree.label}
-                      childNodes={fileTree.childNodes}
-                      type={fileTree.type}
-                      parentNode={fileTree.parentNode} />
-            </ul>
-        )
-    }
+  render() {
+    const fileTree = this.getFileTree()
+    return (
+      <ul>
+        <FileNode label={fileTree.label}
+          childNodes={fileTree.childNodes}
+          type={fileTree.type}
+          parentNode={fileTree.parentNode} />
+      </ul>
+    )
+  }
 }
 
 export default FileTree;
--- electron/src/renderer/project/sidebar/file-tree/node.tsx
+++ electron/src/renderer/project/sidebar/file-tree/node.tsx
@@ -4,58 +4,58 @@ import DoubleClick from "./double-click-HOC";
 export type NodeType = "Datafile" | "Sheet" | "Label" | "Yaml" | "Annotation" | "Wikifier" | "Entity"
 
 export interface NodeProps {
-    label: string;
-    parentNode: NodeProps | null;
-    childNodes: NodeProps[];
-    type: NodeType;
+  label: string;
+  parentNode: NodeProps | null;
+  childNodes: NodeProps[];
+  type: NodeType;
 }
 
 interface NodeState {
-    collapsed: boolean;
+  collapsed: boolean;
 }
 
 class FileNode extends Component<NodeProps, NodeState> {
-    constructor(props: NodeProps) {
-        super(props);
+  constructor(props: NodeProps) {
+    super(props);
 
-        this.state = {
-            collapsed: false,
-        }
+    this.state = {
+      collapsed: false,
+    }
+  }
+
+  onClick() {
+    this.setState({ collapsed: !this.state.collapsed });
+    console.log('clicked', this.state.collapsed)
+  }
+
+  onDoubleClick(e: React.MouseEvent) {
+    console.log("onNodeDoubleClicked: ", e.currentTarget.innerHTML);
+  }
+
+  onRightClick(e: React.MouseEvent) {
+    console.log("onNodeRightClicked: ", e.currentTarget.innerHTML);
+  }
+
+  render() {
+    let childrenNodes = null;
+    if (this.props.childNodes && !this.state.collapsed) {
+      childrenNodes = (<ul>
+        {this.props.childNodes.map((n: NodeProps) => <FileNode key={n.label} label={n.label} childNodes={n.childNodes} parentNode={n.parentNode} type={n.type} />)}
+      </ul>)
     }
 
-    onClick() {
-        this.setState({ collapsed: !this.state.collapsed });
-        console.log('clicked', this.state.collapsed)
-    }
-
-    onDoubleClick(e: React.MouseEvent) {
-        console.log("onNodeDoubleClicked: ", e.currentTarget.innerHTML);
-    }
-
-    onRightClick(e: React.MouseEvent) {
-        console.log("onNodeRightClicked: ", e.currentTarget.innerHTML);
-    }
-
-    render() {
-        let childrenNodes=null;
-        if (this.props.childNodes && !this.state.collapsed) {
-            childrenNodes = (<ul>
-            {this.props.childNodes.map((n: NodeProps) => <FileNode key={n.label} label={n.label} childNodes={n.childNodes} parentNode={n.parentNode} type={n.type} /> )}
-            </ul>)
-            }
-
-        return (
-            <li>
-                <DoubleClick onClick={() => this.onClick()} onDoubleClick={(e) => this.onDoubleClick(e)}>
-                    <label
-                        onContextMenu={(e) => this.onRightClick(e)}>
-                        {this.props.label}
-                    </label>
-                </DoubleClick>
-                {childrenNodes}
-            </li>
-        )
-    }
+    return (
+      <li>
+        <DoubleClick onClick={() => this.onClick()} onDoubleClick={(e) => this.onDoubleClick(e)}>
+          <label
+            onContextMenu={(e) => this.onRightClick(e)}>
+            {this.props.label}
+          </label>
+        </DoubleClick>
+        {childrenNodes}
+      </li>
+    )
+  }
 }
 
 export default FileNode;
