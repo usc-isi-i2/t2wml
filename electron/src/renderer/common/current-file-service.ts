f8534e23b9261b02ea0634a6ddf68109119a93e4
devora <devorawitty@gmail.com>
2021-01-07 12:22:30 +0200

    addition to last commit

--- electron/src/renderer/common/current-file-service.ts
+++ electron/src/renderer/common/current-file-service.ts
@@ -27,7 +27,7 @@ export class CurrentFilesService {
     @observable prevSelections = {};
 
     getAnnotationFileFromProject(): string | undefined {
-        const project = wikiStore.project!.projectDTO;
+        const project = wikiStore.project.projectDTO!;
         if (Object.keys(project.annotations).length && project.annotations[this.currentState.dataFile!]) {
             return project.annotations[this.currentState.dataFile!][this.currentState.sheetName!].val_arr[0];
         }
@@ -79,7 +79,7 @@ export class CurrentFilesService {
 
     @action
     setMappingFiles() {
-        const project = wikiStore.project!.projectDTO;
+        const project = wikiStore.project.projectDTO!;
         const dataFile = this.currentState.dataFile!;
         const sheet = this.currentState.sheetName!;
 
@@ -100,7 +100,7 @@ export class CurrentFilesService {
 
     @action
     changeDataFile(newFile: string) {
-        const project = wikiStore.project!.projectDTO;
+        const project = wikiStore.project.projectDTO!;
         this.currentState.dataFile = newFile;
         this.currentState.sheetName = project.data_files[newFile].val_arr[0];
 
@@ -110,8 +110,7 @@ export class CurrentFilesService {
 
     @action
     changeSheet(newSheet: string, dataFile: string) {
-        debugger
-        const project = wikiStore.project!.projectDTO;
+        const project = wikiStore.project.projectDTO!;
         this.currentState.dataFile = dataFile;
         // If this sheet is not part of current datafile, search the relevant data file.
         if (project.data_files[this.currentState.dataFile].val_arr.indexOf(newSheet) < 0) {
@@ -129,7 +128,7 @@ export class CurrentFilesService {
 
     @action
     changeYaml(newYaml: string, sheetName: string, dataFile: string) {
-        const project = wikiStore.project!.projectDTO;
+        const project = wikiStore.project.projectDTO!;
         this.currentState.dataFile = dataFile;
         this.currentState.sheetName = sheetName;
         // If this yaml is not part of current datafile, search the relevant data file and sheet.
@@ -144,7 +143,6 @@ export class CurrentFilesService {
             }
         }
 
-        debugger
         this.currentState.mappingFile = newYaml;
         this.currentState.mappingType = 'Yaml';
         wikiStore.table.mode = 'Output';
@@ -155,7 +153,7 @@ export class CurrentFilesService {
     @action
     changeYamlInSameSheet(newYaml: string) {
         if (!newYaml) {
-            const project = wikiStore.project!.projectDTO;
+            const project = wikiStore.project.projectDTO!;
             if (Object.keys(project.yaml_sheet_associations).length && project.yaml_sheet_associations[this.currentState.dataFile!]) {
                 newYaml = project.yaml_sheet_associations[this.currentState.dataFile!][this.currentState.sheetName!].val_arr[0];
             }
@@ -170,7 +168,7 @@ export class CurrentFilesService {
 
     @action
     changeAnnotation(newAnnotation: string, sheetName: string, dataFile: string) {
-        const project = wikiStore.project!.projectDTO;
+        const project = wikiStore.project.projectDTO!;
         currentFilesService.currentState.dataFile = dataFile;
         currentFilesService.currentState.sheetName = sheetName;
         // If this yaml is not part of current datafile, search the relevant data file and sheet.
@@ -195,7 +193,7 @@ export class CurrentFilesService {
     @action
     changeAnnotationInSameSheet(newAnnotation?: string) {
         if (!newAnnotation) {
-            const project = wikiStore.project!.projectDTO;
+            const project = wikiStore.project.projectDTO!;
             if (Object.keys(project.annotations).length && project.annotations[this.currentState.dataFile!]) {
                 newAnnotation = project.annotations[this.currentState.dataFile!][this.currentState.sheetName!].val_arr[0];
             } //TODO- if there is no annotation- add one
@@ -207,7 +205,7 @@ export class CurrentFilesService {
     }
 
     saveCurrentFileSelections() {
-        const project = wikiStore.project!.projectDTO;
+        const project = wikiStore.project.projectDTO!;
         const path = `${project.directory}/${filename}`;
         fs.writeFileSync(path, JSON.stringify({
             'currentState': this.currentState,
@@ -253,7 +251,7 @@ export class CurrentFilesService {
     // }
 
     // fillFilesData() {
-    //     const project = wikiStore.project!.projectDTO;
+    //     const project = wikiStore.project.projectDTO!;
     //     this.fillCurrents();
     //     this.fillPrevSelections(project);
     //     this.saveCurrentFileSelections(project.directory);
