57bca4f55688246bac3277b23a0deda17bf30fde
g1eb <mail@g1eb.com>
2021-01-26 11:24:24 -0800

    Don't add the borders on reset of the selections, only on close of the menu

--- electron/src/renderer/project/table/annotation-table/annotation-table.tsx
+++ electron/src/renderer/project/table/annotation-table/annotation-table.tsx
@@ -336,9 +336,6 @@ class AnnotationTable extends Component<{}, TableState> {
       table.querySelectorAll('.cell-border-right').forEach(e => e.remove());
       table.querySelectorAll('.cell-border-bottom').forEach(e => e.remove());
       table.querySelectorAll('.cell-resize-corner').forEach(e => e.remove());
-
-      // Add borders to the annotation blocks
-      this.updateAnnotationBlocks();
     }
   }
 
@@ -876,7 +873,10 @@ class AnnotationTable extends Component<{}, TableState> {
     this.setState({
       showAnnotationMenu: false,
       selectedAnnotationBlock: undefined,
-    }, () => this.resetSelections());
+    }, () => {
+      this.resetSelections();
+      this.updateAnnotationBlocks();
+    });
   }
 
   renderAnnotationMenu() {
