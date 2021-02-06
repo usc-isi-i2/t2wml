d7f1a1989270a803a77a22fd8578edc0c1221474
g1eb <mail@g1eb.com>
2021-02-05 20:57:55 -0800

    Whitespace fixes in the general.ts file

--- electron/src/renderer/common/general.ts
+++ electron/src/renderer/common/general.ts
@@ -1,39 +1,42 @@
 // console.log
 export const LOG = {
-    default: "background: ; color: ",
-    highlight: "background: yellow; color: black",
-    link: "background: white; color: blue"
-};
-
-export interface gridApiInterface {
-    api: any;
-    columnApi: any;
+  default: 'background: ; color: ',
+  highlight: 'background: yellow; color: black',
+  link: 'background: white; color: blue'
 }
 
+
+export interface gridApiInterface {
+  api: any;
+  columnApi: any;
+}
+
+
 export interface ErrorMessage {
-    errorCode: number;
-    errorTitle: string;
-    errorDescription: string;
+  errorCode: number;
+  errorTitle: string;
+  errorDescription: string;
 }
 
 
 export class Cell {
-    constructor(public col: number, public row: number, public value?: string) {
-    }
+  constructor(public col: number, public row: number, public value?: string) {}
 }
+
 
 export enum t2wmlColors {
-    PROJECT = "#f8f9fa",
-    TABLE = "#339966",
-    WIKIFIER = "#006699",
-    YAML = "#006699",
-    OUTPUT = "#990000",
-    TREE = "#695b5a"
+  PROJECT = '#f8f9fa',
+  TABLE = '#339966',
+  WIKIFIER = '#006699',
+  YAML = '#006699',
+  OUTPUT = '#990000',
+  TREE = '#695b5a',
 }
 
+
 export interface CellSelection{
-    x1: number;
-    x2: number;
-    y1: number;
-    y2: number;
+  x1: number;
+  x2: number;
+  y1: number;
+  y2: number;
 }
