08c49c47492592c4ef5f744b9930c124426ea85d
g1eb <mail@g1eb.com>
2021-02-05 17:44:31 -0800

    Check for collisions with the existing annotation blocks when creating new ones

--- electron/src/renderer/project/table/annotation-table/annotation-table.tsx
+++ electron/src/renderer/project/table/annotation-table/annotation-table.tsx
@@ -459,11 +459,56 @@ class AnnotationTable extends Component<{}, TableState> {
     });
   }
 
+  checkOverlaps() {
+    const { x1, y1, x2, y2 } = this.selection;
+
+    // Get the coordinates of the sides
+    const aTop = y1 <= y2 ? y1 : y2;
+    const aLeft = x1 <= x2 ? x1 : x2;
+    const aRight = x2 >= x1 ? x2 : x1;
+    const aBottom = y2 >= y1 ? y2 : y1;
+
+    const { blocks } = wikiStore.annotations;
+    for (let i = 0; i < blocks.length; i++ ) {
+      const other = blocks[i].selection;
+
+      // Get the coordinates of the sides
+      const bTop = other.y1 <= other.y2 ? other.y1 : other.y2;
+      const bLeft = other.x1 <= other.x2 ? other.x1 : other.x2;
+      const bRight = other.x2 >= other.x1 ? other.x2 : other.x1;
+      const bBottom = other.y2 >= other.y1 ? other.y2 : other.y1;
+
+      // check for collisions between area A and B
+      if (aTop > bBottom) {
+        continue;
+      }
+      if (aBottom < bTop) {
+        continue;
+      }
+      if (aLeft > bRight) {
+        continue;
+      }
+      if (aRight < bLeft) {
+        continue;
+      }
+
+      // collision detected
+      return true;
+    }
+
+    // no collisions detected
+    return false;
+  }
+
   handleOnMouseUp(event: MouseEvent) {
     this.selecting = false;
     if ( this.selection ) {
       this.standardizeSelection();
-      this.openAnnotationMenu(event);
+      if ( this.checkOverlaps() ) {
+        this.closeAnnotationMenu();
+      } else {
+        this.openAnnotationMenu(event);
+      }
     }
   }
 
