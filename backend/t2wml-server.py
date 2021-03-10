72959c81ff0f627d5d36d9458d317906cddff6a3
Itay Zandbank <itay@chelem.co.il>
2021-03-10 15:26:42 +0200

    Better dockerfile

--- backend/t2wml-server.py
+++ backend/t2wml-server.py
@@ -4,12 +4,15 @@ import sys
 # Make sure we can import other files from this directory
 #BASEDIR = os.path.abspath(os.path.dirname(__file__))
 #if BASEDIR not in sys.path:
-#    sys.path.append(BASEDIR) 
+#    sys.path.append(BASEDIR)
 
 try:
     port = int(sys.argv[1])
 except:
     port = 13000
+
+# Use argparse to accept one argument: port, default 13000, one --public-server with a default of False, when
+# specified, add host='0.0.0.0' to app.run
 
 from application import app
 if __name__ == '__main__':
