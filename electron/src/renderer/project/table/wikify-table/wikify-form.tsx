80d87cd2238cc48f16c8030f7ed20daa1dacb5da
g1eb <mail@g1eb.com>
2021-04-04 11:53:36 -0700

    Hide property search checkbox and instance of input if selected cell is a property

--- electron/src/renderer/project/table/wikify-table/wikify-form.tsx
+++ electron/src/renderer/project/table/wikify-table/wikify-form.tsx
@@ -182,21 +182,29 @@ class WikifyForm extends React.Component<WikifyFormProperties, WikifyFormState> 
   }
 
   renderSearchInputs() {
-    const { search, instanceOfSearch, qnodes, searchProperties } = this.state;
+    const {
+      qnodes,
+      search,
+      instanceOfSearch,
+      searchProperties,
+      selectedType,
+    } = this.state;
     return (
       <Form.Group as={Row}>
-        <Col sm="12" className="search-properties">
-          <input id="check-property-search"
-            type="checkbox"
-            defaultChecked={searchProperties}
-            onChange={this.toggleSearchProperties.bind(this)} />
-          <Form.Label
-            htmlFor="check-property-search"
-            className="text-muted">
-            Search Properties
-          </Form.Label>
-        </Col>
-        <Col sm="12" md="8">
+        {selectedType !== 'property' && (
+          <Col sm="12" className="search-properties">
+            <input id="check-property-search"
+              type="checkbox"
+              defaultChecked={searchProperties}
+              onChange={this.toggleSearchProperties.bind(this)} />
+            <Form.Label
+              htmlFor="check-property-search"
+              className="text-muted">
+              Search Properties
+            </Form.Label>
+          </Col>
+        )}
+        <Col sm="12" md={selectedType !== 'property' ? '8' : '12'}>
           <Form.Label className="text-muted">Search</Form.Label>
           <Form.Control
             type="text" size="sm"
@@ -211,22 +219,24 @@ class WikifyForm extends React.Component<WikifyFormProperties, WikifyFormState> 
                 onClick={this.clearSearch.bind(this)} />
             ) : null}
         </Col>
-        <Col sm="12" md="4">
-          <Form.Label className="text-muted">Instance Of</Form.Label>
-          <Form.Control
-            type="text" size="sm"
-            placeholder="qnode"
-            value={instanceOfSearch}
-            onChange={(event: any) => {
-              this.handleOnChangeInstanceOfSearch(event)
-            }} />
-            {instanceOfSearch && qnodes.length ? (
-              <FontAwesomeIcon
-                icon={faTimes}
-                className="clear-button"
-                onClick={this.clearInstanceOfSearch.bind(this)} />
-            ) : null}
-        </Col>
+        {selectedType !== 'property' && (
+          <Col sm="12" md="4">
+            <Form.Label className="text-muted">Instance Of</Form.Label>
+            <Form.Control
+              type="text" size="sm"
+              placeholder="qnode"
+              value={instanceOfSearch}
+              onChange={(event: any) => {
+                this.handleOnChangeInstanceOfSearch(event)
+              }} />
+              {instanceOfSearch && qnodes.length ? (
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  className="clear-button"
+                  onClick={this.clearInstanceOfSearch.bind(this)} />
+              ) : null}
+          </Col>
+        )}
       </Form.Group>
     )
   }
