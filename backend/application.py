78ea6256f9faeb8d094b056b275b62b84d4e57e6
devora <devorawitty@gmail.com>
2021-04-22 10:52:34 +0300

    refactor/tidy

--- backend/application.py
+++ backend/application.py
@@ -496,43 +496,7 @@ def update_settings():
     response = dict(project=get_project_dict(project))
     return response, 200
 
-
 @app.route('/api/properties', methods=['GET'])
-@json_response
-def get_properties():
-    q = request.args.get('q')
-    if not q:
-        raise web_exceptions.InvalidRequestException("No search parameter set")
-
-    # construct the url with correct parameters for kgtk search
-    url = 'https://kgtk.isi.edu/api?q={}'.format(q)
-    url += '&type=ngram&extra_info=true&language=en&item=property'
-
-    data_type = request.args.get('data_type')
-    if data_type:
-        if data_type=="wikibaseitem":
-            data_type="wikibase-item"
-        url += '&data_type={}'.format(data_type)
-
-    try:
-        response = requests.get(url, verify=False)
-    except requests.exceptions.RequestException as error:
-        raise web_exceptions.InvalidRequestException(error)
-    else:
-        items = response.json()
-        if type(items) != list:
-            raise web_exceptions.InvalidRequestException(
-                "KGTK did not return a valid list of properties"
-            )
-        properties = [{
-            'id': item['qnode'],
-            'label': item['label'][0] if item['label'] else '',
-            'description': item['description'][0] if item['description'] else '',
-        } for item in items]
-
-    return {'qnodes': properties}, 200
-
-
 @app.route('/api/qnodes', methods=['GET'])
 @json_response
 def get_qnodes():
@@ -543,16 +507,25 @@ def get_qnodes():
     # construct the url with correct parameters for kgtk search
     url = 'https://kgtk.isi.edu/api?q={}'.format(q)
 
-    # get the optional parameters for the url
-    is_class = request.args.get('is_class')
-    url += '&extra_info=true&language=en'
-    if is_class:
-        url += '&is_class=true&type=exact&size=5'
-    else:
-        url += '&is_class=false&type=ngram&size=10'
-        instance_of = request.args.get('instance_of')
-        if instance_of:
-            url += '&instance_of={}'.format(instance_of)
+    if "properties" in request.url:
+        url += '&type=ngram&extra_info=true&language=en&item=property'
+        data_type = request.args.get('data_type')
+        if data_type:
+            if data_type=="wikibaseitem":
+                data_type="wikibase-item"
+        url += '&data_type={}'.format(data_type)
+
+    else: #qnodes
+        # get the optional parameters for the url
+        is_class = request.args.get('is_class')
+        url += '&extra_info=true&language=en'
+        if is_class:
+            url += '&is_class=true&type=exact&size=5'
+        else:
+            url += '&is_class=false&type=ngram&size=10'
+            instance_of = request.args.get('instance_of')
+            if instance_of:
+                url += '&instance_of={}'.format(instance_of)
 
     try:
         response = requests.get(url, verify=False)
@@ -562,7 +535,7 @@ def get_qnodes():
         items = response.json()
         if type(items) != list:
             raise web_exceptions.InvalidRequestException(
-                "KGTK did not return a valid list of qnodes"
+                "KGTK did not return a valid list of nodes"
             )
         qnodes = [{
             'id': item['qnode'],
