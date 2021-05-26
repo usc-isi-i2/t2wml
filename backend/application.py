8ec4d548a2740b3a1d062e6661756096200563e7
devora <devorawitty@gmail.com>
2021-05-26 13:47:47 +0300

    tweak imports

--- backend/application.py
+++ backend/application.py
@@ -8,9 +8,7 @@ from t2wml.wikification.utility_functions import dict_to_kgtk, kgtk_to_dict
 import web_exceptions
 from app_config import app
 from werkzeug.utils import secure_filename
-from t2wml.api import add_entities_from_file
-from t2wml.input_processing.annotation_suggesting import annotation_suggester
-from t2wml.input_processing.annotation_parsing import get_Pnode, get_Qnode
+from t2wml.api import add_entities_from_file, annotation_suggester, get_Pnode, get_Qnode
 from t2wml_web import (get_kgtk_download_and_variables, set_web_settings, download, get_layers, get_annotations, get_table, save_annotations,
                        get_project_instance, create_api_project, get_partial_csv, get_qnodes_layer, get_entities, suggest_annotations, update_entities, update_t2wml_settings, wikify, get_entities)
 from utils import (file_upload_validator, get_empty_layers, save_dataframe,
@@ -655,7 +653,7 @@ def create_qnode():
         id = get_Pnode(project, label)
     else:
         id = get_Qnode(project, label)
-        
+
     entity_dict={
         "label":label
     }
