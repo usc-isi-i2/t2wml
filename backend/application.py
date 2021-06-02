1ccdf1f4c9e1380eb1947c812ba7d97ea8815174
devora <devorawitty@gmail.com>
2021-06-02 13:34:43 +0300

    add back in some more changes

--- backend/application.py
+++ backend/application.py
@@ -670,8 +670,9 @@ def create_qnode():
     }
     if is_prop:
         entity_dict["data_type"]=data_type
+    entity_dict["description"] = request_json.get("description", "")
 
-    for key in ["description", "P31"]: #more to be added
+    for key in ["P31"]: #may add more
         if request_json.get(key, None):
             entity_dict[key]=request_json[key]
 
@@ -682,7 +683,7 @@ def create_qnode():
     project.save()
     t2wml_settings.wikidata_provider.save_entry(**entity_dict)
 
-    response=dict(id=node_id, project=get_project_dict(project))
+    response=dict(entity=entity_dict, project=get_project_dict(project))
 
     selection=request_json.get("selection", None)
     if selection:
