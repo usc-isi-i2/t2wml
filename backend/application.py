0fbb5bd3d9876a9a1d975170099a49a6fdb00c3d
devora <devorawitty@gmail.com>
2021-06-21 12:16:12 +0300

    undo change

--- backend/application.py
+++ backend/application.py
@@ -766,14 +766,16 @@ def create_qnode():
     response = dict(entity=entity_dict, project=get_project_dict(project))
 
     selection = request_json.get("selection", None)
-    calc_params = get_calc_params(project)
     if selection:
+        calc_params = get_calc_params(project)
         context = request.get_json().get("context", "")
         (col1, row1), (col2, row2) = selection
         value = calc_params.sheet[row1, col1]
         create_user_wikification(calc_params, project, selection, value,
                                  context, node_id)
-    response["layers"] = get_qnodes_layer(calc_params)
+        response["layers"] = get_qnodes_layer(calc_params)
+    else:
+        response["layers"] = {}
     return response, 200
 
 
