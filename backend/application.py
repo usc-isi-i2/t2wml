f36078d9aaaa0eeb57cbdcb17291bd2b03486a1c
devora <devorawitty@gmail.com>
2021-07-22 16:42:12 +0300

    move wikifier functions to be next to each other

--- backend/application.py
+++ backend/application.py
@@ -288,12 +288,34 @@ def upload_wikifier_output():
         response["layers"] = get_qnodes_layer(calc_params)
     return response, 200
 
+@app.route('/api/web/wikify_region', methods=['POST']) #V
+@json_response
+def causx_wikify():
+    project = get_project()
+    region = request.get_json()["selection"]
+    overwrite_existing = request.get_json().get("overwrite", False)
+    #context = request.get_json()["context"]
+    calc_params = get_calc_params(project)
+
+    cell_qnode_map, problem_cells = wikify_countries(calc_params, region)
+    project.add_df_to_wikifier_file(calc_params.data_path, cell_qnode_map, overwrite_existing)
+
+    calc_params = get_calc_params(project)
+    response = dict(project=get_project_dict(project))
+    response["layers"] = get_qnodes_layer(calc_params)
+
+    if problem_cells:
+        response['wikifierError'] = "Failed to wikify: " + \
+            ",".join(problem_cells)
+
+    return response, 200
+
 
 @app.route('/api/wikifier_service', methods=['POST'])
 @json_response
 def call_wikifier_service():
     """
-    This function calls the wikifier service to wikifiy a region, and deletes/updates wiki region file's results
+    This function calls the wikifier service to wikify a region, and deletes/updates wiki region file's results
     :return:
     """
     project = get_project()
@@ -837,30 +859,6 @@ def add_mapping_file():
 
     project.save()
     response = dict(project=get_project_dict(project), filename=filename)
-    return response, 200
-
-
-
-@app.route('/api/web/wikify_region', methods=['POST']) #V
-@json_response
-def causx_wikify():
-    project = get_project()
-    region = request.get_json()["selection"]
-    overwrite_existing = request.get_json().get("overwrite", False)
-    #context = request.get_json()["context"]
-    calc_params = get_calc_params(project)
-
-    cell_qnode_map, problem_cells = wikify_countries(calc_params, region)
-    project.add_df_to_wikifier_file(calc_params.data_path, cell_qnode_map, overwrite_existing)
-
-    calc_params = get_calc_params(project)
-    response = dict(project=get_project_dict(project))
-    response["layers"] = get_qnodes_layer(calc_params)
-
-    if problem_cells:
-        response['wikifierError'] = "Failed to wikify: " + \
-            ",".join(problem_cells)
-
     return response, 200
 
 
