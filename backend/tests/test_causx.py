0f95726938c7d473a630f0550813d676b44706b5
devora <devorawitty@gmail.com>
2021-08-19 11:24:18 +0300

    try changing test scope?

--- backend/tests/test_causx.py
+++ backend/tests/test_causx.py
@@ -4,7 +4,7 @@ import json
 from causx_application import app
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def client(request):
     with app.test_client() as client:
         yield client
