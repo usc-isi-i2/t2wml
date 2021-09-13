64ad41d5719811b0deb3c1310900232cc8e3d9b4
devora <devorawitty@gmail.com>
2021-09-13 15:42:04 +0300

    wip commit

--- backend/tests/basic_test.py
+++ backend/tests/basic_test.py
@@ -17,7 +17,7 @@ def get_data(data):
     return data
 
 
-class TestBasicWorkflow(BaseClass):
+class TestBasicYamlWorkflow(BaseClass):
     files_dir=os.path.join(os.path.dirname(__file__), "files_for_tests", "aid")
     expected_results_path=os.path.join(files_dir, "results.json")
     results_dict={}
@@ -38,7 +38,6 @@ class TestBasicWorkflow(BaseClass):
         data = response.data.decode("utf-8")
         data = get_data(data)
         assert data["project"]["directory"]==project_folder
-
 
     def test_03_add_data_file(self, client):
         filename=os.path.join(self.files_dir, self.data_file)
@@ -88,7 +87,6 @@ class TestBasicWorkflow(BaseClass):
         self.compare_jsons(data, 'add_yaml')
 
     def test_11_get_download(self, client):
-        #GET '/api/project/{project_folder}/download/<filetype>'
         url=url_builder(f'/api/project/export/tsv', project_folder, self.data_file, self.sheet_name, self.yaml_file)
         tf =   os.path.join(tempfile.gettempdir(), os.urandom(24).hex())
         response=client.post(url, json=dict(filepath=tf))
@@ -101,6 +99,7 @@ class TestBasicWorkflow(BaseClass):
                 if e_row_dict["id"] in ["47-label", "475-label", "103-label", "782-label", "22-label", "73-label"]:
                     continue
             assert e_row_dict==o_row_dict
+
     def test_12_change_sheet(self, client):
         #GET /api/data/{project_folder}/<sheet_name>
         url=url_builder('/api/table', project_folder, self.data_file, "Sheet4")
@@ -165,27 +164,62 @@ class TestBasicWorkflow(BaseClass):
             json.dump(self.results_dict, f, sort_keys=True, indent=4)
 
 
+class TestBasicAnnotationWorkflow(BaseClass):
+    files_dir=os.path.join(os.path.dirname(__file__), "files_for_tests", "copy_annotations_desktop_version")
 
-class TestLoadingProject(BaseClass):
-    files_dir=os.path.join(os.path.dirname(__file__), "files_for_tests", "aid")
-    expected_results_path=os.path.join(files_dir, "project_results.json")
-    results_dict={}
+    def test_load_project(self, client):
+        #GET f'/api/project?project_folder={self.files_dir}'
+        pass
 
-    def test_11_get_loaded_yaml_files(self, client):
-        url=url_builder('/api/table', self.files_dir, "dataset.xlsx", "Sheet3", "test.yaml")
-        response=client.get(url)
-        data = response.data.decode("utf-8")
-        data = get_data(data)
-        data.pop('project', None)
-        self.results_dict['load_from_path']=data
-        #with open(self.expected_results_path, 'w') as f:
-        #    json.dump(self.results_dict, f, sort_keys=False, indent=4)
-        assert data["layers"]["error"]==self.results_dict['load_from_path']["layers"]["error"]
-        assert data["layers"]["statement"]==self.results_dict['load_from_path']["layers"]["statement"]
-        assert data["layers"]["qnode"]==self.results_dict['load_from_path']["layers"]["qnode"]
-        assert data["layers"]["type"]==self.results_dict['load_from_path']["layers"]["type"]
-        #self.compare_jsons(data, 'load_from_path')
+    def test_create_annotation(self, client):
+        '"POST //api/annotation/create?project_folder={self.files_dir} HTTP/1.1"'
 
+    def test_suggest_annotation(self, client):
+        '/api/annotation/guess-blocks?project_folder={self.files_dir}&data_file=BCcountries.xlsx&sheet_name=B-countries&mapping_file=annotations/BCcountries-B-countries.annotation&mapping_type=Annotation&map_start=0&map_end=50&data_start=0&data_end=50'
+        pass
+
+    def test_wikifiy_country_region(self, client):
+        "POST //api/web/wikify_region?project_folder={self.files_dir}&data_file=BCcountries.xlsx&sheet_name=B-countries&map_start=0&map_end=50&data_start=0&data_end=50 HTTP/1.1"
+        payload={"selection":{"x1":1,"x2":1,"y1":2,"y2":4}}
+
+    def test_get_partial_csv(self, client):
+        "GET //api/partialcsv?project_folder={self.files_dir}&data_file=BCcountries.xlsx&sheet_name=B-countries&mapping_file=annotations/BCcountries-B-countries.annotation&mapping_type=Annotation HTTP/1.1"
+
+    def test_auto_create_nodes(self, client):
+        "POST http://localhost:13000//api/auto_wikinodes?project_folder={self.files_dir}&data_file=BCcountries.xlsx&sheet_name=B-countries&map_start=0&map_end=52&data_start=0&data_end=52"
+        payload={"selection":{"x1":1,"x2":1,"y1":2,"y2":4},"is_property":False}
+
+    def test_search_pnode(self, client):
+        "GET //api/properties?q=quantity HTTP/1.1"
+
+    def test_create_node(self, client):
+        "POST //api/create_node?project_folder={self.files_dir}&data_file=BCcountries.xlsx&sheet_name=B-countries&map_start=0&map_end=53&data_start=0&data_end=53 HTTP/1.1"
+        payload={"is_property":True,"label":"custom-quantity","description":"","data_type":"quantity"}
+
+    def test_copy_annotation_1(self, client):
+        "http://localhost:13000//api/annotation/copy?project_folder={self.files_dir}&map_start=0&map_end=50&data_start=0&data_end=50"
+        payload = {"source":{"dir":self.files_dir,"dataFile":"BCcountries.xlsx","sheetName":"B-countries","annotation":"annotations/BCcountries-B-countries.annotation"},
+                    "destination":{"dir":self.files_dir,"dataFile":"BCcountries.xlsx","sheetName":"C-countries","annotation":"annotations/BCcountries-C-countries.annotation"}}
+
+    def test_copy_annotation_2(self, client):
+        "http://localhost:13000//api/annotation/copy?project_folder={self.files_dir}&map_start=0&map_end=50&data_start=0&data_end=50"
+        payload = {"source":{"dir":self.files_dir,"dataFile":"BCcountries.xlsx","sheetName":"C-countries","annotation":"annotations/BCcountries-C-countries.annotation"},"destination":{"dir":self.files_dir,"dataFile":"countries with ID.csv","sheetName":"","annotation":"annotations/countries with ID-countries with ID.csv.annotation"}}
+
+    def test_edit_annotation(self, client):
+        "http://localhost:13000//api/annotation?project_folder={self.files_dir}&data_file=countries%20with%20ID.csv&sheet_name=countries%20with%20ID.csv&map_start=0&map_end=50&data_start=0&data_end=50"
+        payload={"annotations":[{"id":"cb00d0e9-f4e6-4ded-a3c8-19aa8941762f","link":"","links":{},"property":{"data_type":"time","description":"time and date something took place, existed or a statement was true","id":"P585","label":"point in time"},"role":"qualifier","selection":{"x1":3,"x2":8,"y1":1,"y2":1},"type":"time"},{"id":"7b0772cd-c6e4-4db1-9a52-bc2f975adbf6","link":"60b0a16c-802d-4943-8b79-7bdb4c248bc5","links":{},"role":"mainSubject","selection":{"x1":1,"x2":1,"y1":2,"y2":6},"type":"wikibaseitem"},{"id":"60b0a16c-802d-4943-8b79-7bdb4c248bc5","link":"","links":{"mainSubject":"7b0772cd-c6e4-4db1-9a52-bc2f975adbf6"},"property":{"data_type":"quantity","description":"","id":"PCustomNode-customquantity","label":"custom-quantity"},"role":"dependentVar","selection":{"x1":3,"x2":8,"y1":2,"y2":6},"type":"quantity","selectedArea":"C2:H6"}],"title":"annotations/countries with ID-countries with ID.csv.annotation"}
+
+    def test_suggest_block(self, client):
+        "PUT http://localhost:13000//api/annotation/suggest?project_folder={self.files_dir}&data_file=countries%20with%20ID.csv&sheet_name=countries%20with%20ID.csv"
+        payload={"selection":{"x1":2,"x2":2,"y1":2,"y2":6},"annotations":[{"id":"7b0772cd-c6e4-4db1-9a52-bc2f975adbf6","link":"60b0a16c-802d-4943-8b79-7bdb4c248bc5","links":{},"role":"mainSubject","selection":{"x1":1,"x2":1,"y1":2,"y2":6},"type":"wikibaseitem"},{"id":"60b0a16c-802d-4943-8b79-7bdb4c248bc5","link":"","links":{"mainSubject":"7b0772cd-c6e4-4db1-9a52-bc2f975adbf6"},"property":{"data_type":"quantity","description":"","id":"PCustomNode-customquantity","label":"custom-quantity"},"role":"dependentVar","selection":{"x1":3,"x2":8,"y1":2,"y2":6},"type":"quantity"},{"id":"cb00d0e9-f4e6-4ded-a3c8-19aa8941762f","link":"","links":{},"property":{"data_type":"time","description":"time and date something took place, existed or a statement was true","id":"P585","label":"point in time"},"role":"qualifier","selection":{"x1":3,"x2":8,"y1":1,"y2":1},"type":"time"}]}
+
+    def test_rename_file(self, client):
+        "POST http://localhost:13000//api/files/rename?project_folder={self.files_dir}"
+        payload={"old_name":"horizontal data.xlsx","new_name":"test.xlsx"}
+
+    def test_delete_file(self, client):
+        "POST http://localhost:13000//api/files/delete?project_folder={self.files_dir}"
+        payload={"file_name":"horizontal data.xlsx","delete":False}
 
 class TestCleaning(BaseClass):
     files_dir=os.path.join(os.path.dirname(__file__), "files_for_tests", "homicide")
@@ -199,5 +233,3 @@ class TestCleaning(BaseClass):
         assert cleaned_entries[1] == {'cleaned': '200', 'indices': [[6,3]], 'original': '4'}
 
 
-class TestCopyAnnotation(BaseClass):
-    pass
